47a93d3268e1a84e0695ce92cbd33c4e7ae03780
Anurag Priyam <anurag08priyam@gmail.com>
2010-10-04 22:19:50 +0530

    defined post /, uri. which blasts the input using the shell commands.

--- search.rb
+++ search.rb
@@ -1,7 +1,28 @@
 # search.rb
 require 'rubygems'
 require 'sinatra'
+require 'tempfile'
+
+NDB = '/home/yeban/code/Sinatra/simpleSequenceSearchServer/db/nucleotide/Sinvicta2-2-3.cdna.subset.fasta'
+PDB = '/home/yeban/code/Sinatra/simpleSequenceSearchServer/db/protein/Sinvicta2-2-3.prot.subset.fasta'
 
 get '/' do
   erb :search
+end
+
+post '/' do
+  method = params[ :method ]
+  puts params[ :database ]
+  database = if params[ :database ] == 'protein'
+               PDB
+             elsif params[ :database ] == 'nucleotide'
+               NDB
+             end
+
+  tmp = Tempfile.new("seqfile")
+  tmp.puts( params[ :sequence ] )
+  tmp.close
+  report = `#{method} -db #{database} -query #{tmp.path}`
+  tmp.delete
+  report
 end
