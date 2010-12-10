46a66b9bc1f4842cedeefee0c2cc3e6d89dfb8c6
Anurag Priyam <anurag08priyam@gmail.com>
2010-12-10 09:50:10 +0530

    trigger an xhr request on paste and get back the sequence type; accordingly disable or enable blast methods (will disable all methods if first protein is pasted and then nucleotide, or vice versa)

--- public/js/search.js
+++ public/js/search.js
@@ -2,5 +2,27 @@ $(document).ready(function(){
     $("fieldset.advanced").click(function(event){
         $(this).find("pre").toggle("fast");
     });
+
+    //var seq;
+    $("#sequence:first").bind("paste", function(event){
+        //get the value of the sequence
+        var element = $(this);
+        setTimeout(function(){
+            var seq = element.val();
+            var seq_type;
+            $.post('/ajax', {sequence: seq}, function(data){
+                seq_type = data;
+                if (seq_type == "nucleotide"){
+                    $("#blastp").attr('disabled', 'disabled');
+                    $("#tblastn").attr('disabled', 'disabled');
+                }
+                else if (seq_type == "protein"){
+                    $("#blastn").attr('disabled', 'disabled');
+                    $("#tblastx").attr('disabled', 'disabled');
+                    $("#blastx").attr('disabled', 'disabled');
+                }
+            });
+        }, 10);
+    });
 });
 
