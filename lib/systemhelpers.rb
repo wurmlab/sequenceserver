f2baa5ddea935fdf7c510977fa9d86097dd4cbd8
Anurag Priyam <anurag08priyam@gmail.com>
2011-02-02 23:15:37 +0530

    allow SystemHelpers module to be used independantly
    Let the methods of SystemHelpers be available both as instance methods,
    and module methods (like the standard Math module); provides for the
    flexibility of using the module as a mixin or standalone.

--- lib/systemhelpers.rb
+++ lib/systemhelpers.rb
@@ -2,6 +2,8 @@
 # copyright yannick wurm and anurag priyam
 
 module SystemHelpers
+  # make all the instance methods available as module methods too
+  extend self
 
   # check if the given command exists and is executable
   # returns True if all is good.
