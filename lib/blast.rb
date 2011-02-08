3c0c9686294b699ef9514769271a7cd574e06057
Anurag Priyam <anurag08priyam@gmail.com>
2011-02-09 05:07:22 +0530

    use SequenceServer namespace for the library
    1e244d16300ec introduced SequnceServer namespace but it was not used
    until now.

--- lib/blast.rb
+++ lib/blast.rb
@@ -1,159 +1,161 @@
 require 'tempfile'
 require 'open3'
 
-# Simple ncbi-blast wrapper. Check examples below.
-class Blast
-  # blast method
-  attr_accessor :method
+module SequenceServer
+  # Simple ncbi-blast wrapper. Check examples below.
+  class Blast
+    # blast method
+    attr_accessor :method
 
-  # database name
-  attr_accessor :db
+    # database name
+    attr_accessor :db
 
-  # query sequence string
-  attr_accessor :qstring
-  
-  # query file name
-  attr_accessor :qfile
+    # query sequence string
+    attr_accessor :qstring
 
-  # advanced blast options
-  attr_accessor :options
+    # query file name
+    attr_accessor :qfile
 
-  # command string to be executed
-  attr_reader   :command
+    # advanced blast options
+    attr_accessor :options
 
-  # result of executing command
-  attr_reader   :result
+    # command string to be executed
+    attr_reader   :command
 
-  # errors if any while executing command
-  attr_reader   :error
+    # result of executing command
+    attr_reader   :result
 
-  # Initialize a new blast search.
-  # ---
-  # Arguments(optional):
-  # * method(String)  - blast executable (shell executable, or absolute path)
-  # * db(String)      - database name as returned by 'blastdbcmd -list'
-  # * query(Hash)     - query string/file, and options.
-  #
-  # In the query Hash, use:
-  # * :qfile(String)   - to run Blast against a file.
-  # * :qstrin(String)  - to run Blast against a string.
-  # * :options(String) - to specify multiple blast options.
-  #
-  # Either :qfile, or :qstring should be used. If both are given, by design
-  # :qstring will be used to run blast.
-  # ---
-  # Examples:
-  #
-  #   b = Blast.new("blastn", "S.cdna.fasta", :qfile => 'query.seq', :options => "-html -num_threads 4")
-  #   b = Blast.new("blastn", "S.cdna.fasta", :qstring => 'ATGTCCGCGAATCGATTGAACGTGCTGGTGACCCTGATGCTCGCCGTCGCGCTTCTTGTG')
-  #
-  #   b.run!        => true
-  #   b.result      => "blast output"
-  #
-  #   # change the blast method.
-  #   b.method = 'blastp'
-  #
-  #   b.run!        => false
-  #   b.error       => "blast error output"
-  def initialize(method = nil, db = nil, query = {})
-    @method  = method
-    @db      = db
-    @qstring = query[:qstring]
-    @qfile   = query[:qfile]
-    @options = query[:options]
-  end
+    # errors if any while executing command
+    attr_reader   :error
 
-  # Run blast everytime it is called. Returns the success
-  # status - true, or false. Blast method, db, and qfile/qstring
-  # need to be set before calling this method, else blast will fail.
-  #
-  #   b = Blast.new
-  #   b.run!   => false
-  #
-  #   # set blast method, and db
-  #   b.method = 'blastn'
-  #   b.db     = 'S.cdna.fasta'
-  #
-  #   b.run!   => false
-  #   b.errors => "blast error output"
-  #
-  #   # set qfile
-  #   b.qfile  = 'query1.seq'    
-  #
-  #   b.run!   => true
-  #   b.reuslt => "blast output"
-  def run!
-    # can not run blast if method is not specified
-    return false unless @method
-
-    # create a tempfile if qstring is given
-    if @qstring
-      @tempfile = Tempfile.new('qfile')
-      @tempfile.puts(qstring)
-      @tempfile.close
-      @qfile    = @tempfile.path
+    # Initialize a new blast search.
+    # ---
+    # Arguments(optional):
+    # * method(String)  - blast executable (shell executable, or absolute path)
+    # * db(String)      - database name as returned by 'blastdbcmd -list'
+    # * query(Hash)     - query string/file, and options.
+    #
+    # In the query Hash, use:
+    # * :qfile(String)   - to run Blast against a file.
+    # * :qstrin(String)  - to run Blast against a string.
+    # * :options(String) - to specify multiple blast options.
+    #
+    # Either :qfile, or :qstring should be used. If both are given, by design
+    # :qstring will be used to run blast.
+    # ---
+    # Examples:
+    #
+    #   b = Blast.new("blastn", "S.cdna.fasta", :qfile => 'query.seq', :options => "-html -num_threads 4")
+    #   b = Blast.new("blastn", "S.cdna.fasta", :qstring => 'ATGTCCGCGAATCGATTGAACGTGCTGGTGACCCTGATGCTCGCCGTCGCGCTTCTTGTG')
+    #
+    #   b.run!        => true
+    #   b.result      => "blast output"
+    #
+    #   # change the blast method.
+    #   b.method = 'blastp'
+    #
+    #   b.run!        => false
+    #   b.error       => "blast error output"
+    def initialize(method = nil, db = nil, query = {})
+      @method  = method
+      @db      = db
+      @qstring = query[:qstring]
+      @qfile   = query[:qfile]
+      @options = query[:options]
     end
 
-    # form command to execute
-    @command = to_s
+    # Run blast everytime it is called. Returns the success
+    # status - true, or false. Blast method, db, and qfile/qstring
+    # need to be set before calling this method, else blast will fail.
+    #
+    #   b = Blast.new
+    #   b.run!   => false
+    #
+    #   # set blast method, and db
+    #   b.method = 'blastn'
+    #   b.db     = 'S.cdna.fasta'
+    #
+    #   b.run!   => false
+    #   b.errors => "blast error output"
+    #
+    #   # set qfile
+    #   b.qfile  = 'query1.seq'    
+    #
+    #   b.run!   => true
+    #   b.reuslt => "blast output"
+    def run!
+      # can not run blast if method is not specified
+      return false unless @method
 
-    # execute command and capture both stdout, and stderr
-    Open3.popen3(@command) do |stdin, stdout, stderr|
-      @result = stdout.readlines # convert to string?
-      @error  = stderr.readlines
+      # create a tempfile if qstring is given
+      if @qstring
+        @tempfile = Tempfile.new('qfile')
+        @tempfile.puts(qstring)
+        @tempfile.close
+        @qfile    = @tempfile.path
+      end
+
+      # form command to execute
+      @command = to_s
+
+      # execute command and capture both stdout, and stderr
+      Open3.popen3(@command) do |stdin, stdout, stderr|
+        @result = stdout.readlines # convert to string?
+        @error  = stderr.readlines
+      end
+
+      # set and return success status
+      return @success = @error.empty?
+
+    ensure
+      # delete tempfile if it was created
+      @tempfile.unlink if @tempfile
     end
 
-    # set and return success status
-    return @success = @error.empty?
-
-  ensure
-    # delete tempfile if it was created
-    @tempfile.unlink if @tempfile
-  end
-
-  # Return the blast type used as a String.
-  #
-  #   b.method = '/home/yeban/opt/blastn'
-  #   b.type   => 'blastn'
-  def type
-    @type ||= @method[(@method.rindex('/') + 1)..-1]
-  end
-
-  # Return success status - true, false, or nil.
-  # 'nil' implies that blast has not been run yet.
-  def success?
-    @success
-  end
-
-  # String representation of the blast object - same as
-  # the command to be executed.
-  def to_s
-    s = "#@method "
-    s << "-db '#@db' " if @db
-    s << "-query #@qfile " if @qfile
-    s << @options.to_s if @options
-    s
-  end
-
-  # Especially helpful in irb - "status : command"
-  def inspect
-    return to_s if success?.nil?
-    (success? ? "success : " : "fail : ") + @command
-  end
-
-  class << self
-    # shortcut method to run blast against a query file
-    def blast_file(method, db, qfile, options = nil)
-      b = Blast.new(method, db, :qfile => qfile, :options => options)
-      b.run!
-      b
+    # Return the blast type used as a String.
+    #
+    #   b.method = '/home/yeban/opt/blastn'
+    #   b.type   => 'blastn'
+    def type
+      @type ||= @method[(@method.rindex('/') + 1)..-1]
     end
 
-    # shortcut method to run blast against a query string
-    def blast_string(method, db, qstring, options = nil)
-      b = Blast.new(method, db, :qstring => qstring, :options => options)
-      b.run!
-      b
+    # Return success status - true, false, or nil.
+    # 'nil' implies that blast has not been run yet.
+    def success?
+      @success
+    end
+
+    # String representation of the blast object - same as
+    # the command to be executed.
+    def to_s
+      s = "#@method "
+      s << "-db '#@db' " if @db
+      s << "-query #@qfile " if @qfile
+      s << @options.to_s if @options
+      s
+    end
+
+    # Especially helpful in irb - "status : command"
+    def inspect
+      return to_s if success?.nil?
+      (success? ? "success : " : "fail : ") + @command
+    end
+
+    class << self
+      # shortcut method to run blast against a query file
+      def blast_file(method, db, qfile, options = nil)
+        b = Blast.new(method, db, :qfile => qfile, :options => options)
+        b.run!
+        b
+      end
+
+      # shortcut method to run blast against a query string
+      def blast_string(method, db, qstring, options = nil)
+        b = Blast.new(method, db, :qstring => qstring, :options => options)
+        b.run!
+        b
+      end
     end
   end
 end
--- lib/sequencehelpers.rb
+++ lib/sequencehelpers.rb
@@ -1,95 +1,97 @@
-# Module to collect some sequence-related helper functions
-module SequenceHelpers
+module SequenceServer
+  # Module to collect some sequence-related helper functions
+  module SequenceHelpers
 
-  # copied from bioruby's Bio::Sequence
-  # returns a Hash. Eg: composition("asdfasdfffffasdf")
-  #                      => {"a"=>3, "d"=>3, "f"=>7, "s"=>3} 
-  def composition(sequence_string)
-    count = Hash.new(0)
-    sequence_string.scan(/./) do |x|
-      count[x] += 1
-    end
-    return count
-  end
-
-  # Strips all non-letter characters. guestimates sequence based on that.
-  # If less than 10 useable characters... returns nil
-  # If more than 90% ACGTU returns :nucleotide. else returns :protein
-  def guess_sequence_type(sequence_string)
-    cleaned_sequence = sequence_string.gsub(/[^A-Z]/i, '')  # removing non-letter characters
-    cleaned_sequence.gsub!(/[NX]/i, '')                     # removing ambiguous  characters
-
-    return nil if cleaned_sequence.length < 10 # conservative 
-     
-    composition = composition(cleaned_sequence)       
-    composition_NAs    = composition.select { |character, count|character.match(/[ACGTU]/i) } # only putative NAs
-    putative_NA_counts = composition_NAs.collect { |key_value_array| key_value_array[1] }     # only count, not char
-    putative_NA_sum    = putative_NA_counts.inject { |sum, n| sum + n }                       # count of all putative NA
-    putative_NA_sum    = 0 if putative_NA_sum.nil?
-
-    if putative_NA_sum > (0.9 * cleaned_sequence.length)
-      return :nucleotide
-    else
-      return :protein
-    end
-  end
-
-  # splits input at putative fasta definition lines (like ">adsfadsf"), guesses sequence type for each sequence. 
-  # if not enough sequence to determine, returns nil.
-  # if 2 kinds of sequence mixed together, raises ArgumentError
-  # otherwise, returns :nucleotide or :protein
-  def type_of_sequences(fasta_format_string)
-    # the first sequence does not need to have a fasta definition line
-    sequences = fasta_format_string.split(/^>.*$/).delete_if { |seq| seq.empty? }
-    
-    # get all sequence types
-    sequence_types = sequences.collect { |seq|  guess_sequence_type(seq) }.uniq.compact
-    
-    return nil if sequence_types.empty?
-
-    if sequence_types.length == 1
-      return sequence_types.first # there is only one (but yes its an array)
-    else 
-      raise ArgumentError, "Insufficient info to determine sequence type. Cleaned queries are: #{ sequences.to_s }"
-    end
-  end
-
-  # Return the database type that can be used for a given blast method.
-  #   db_type_for("blastp")  => :protein
-  #   db_type_for("tblastn") => :nucleotide
-  def db_type_for(blast_method)
-    case blast_method
-    when 'blastp', 'blastx'
-      :protein
-    when 'blastn', 'tblastx', 'tblastn'
-      :nucleotide
-    end
-  end
-
-  # Return the blast methods that can be used for a given type of sequence.
-  #   blast_methods_for(:protein)     => ['blastp', 'tblastn']
-  #   blast_methods_for(:nucleotide)  => ['blastn','tblastx','blastx']
-  def blast_methods_for(seq_type)
-    case seq_type
-    when :protein
-      ['blastp', 'tblastn']
-    when :nucleotide
-      ['blastn','tblastx','blastx']
-    end
-  end
-
-  def sequence_from_blastdb(identifiers, db)  # helpful when displaying parsed blast results
-    entries_to_get = identifiers           if identifiers.class == String
-    entries_to_get = identifiers.join(',') if identifiers.class == Array
-    raise ArgumentError, "No ids to fetch: #{identifiers.to_s}" if entries_to_get.empty?
-
-    sequences = %x|blastdbcmd -db #{db} -entry #{entries_to_get} 2>&1|
-    if sequences.include?("No valid entries to search") 
-      raise ArgumentError, "Cannot find ids: #{entries_to_get} in #{db}." +
-      "OR makeblastdb needs to be rerun with '-parse_seqids'"
+    # copied from bioruby's Bio::Sequence
+    # returns a Hash. Eg: composition("asdfasdfffffasdf")
+    #                      => {"a"=>3, "d"=>3, "f"=>7, "s"=>3} 
+    def composition(sequence_string)
+      count = Hash.new(0)
+      sequence_string.scan(/./) do |x|
+        count[x] += 1
+      end
+      return count
     end
 
-    sequences.chomp + "\n"  # fastaformat in a string - not sure blastdbcmd includes newline
-  end
+    # Strips all non-letter characters. guestimates sequence based on that.
+    # If less than 10 useable characters... returns nil
+    # If more than 90% ACGTU returns :nucleotide. else returns :protein
+    def guess_sequence_type(sequence_string)
+      cleaned_sequence = sequence_string.gsub(/[^A-Z]/i, '')  # removing non-letter characters
+      cleaned_sequence.gsub!(/[NX]/i, '')                     # removing ambiguous  characters
 
+      return nil if cleaned_sequence.length < 10 # conservative 
+
+      composition = composition(cleaned_sequence)       
+      composition_NAs    = composition.select { |character, count|character.match(/[ACGTU]/i) } # only putative NAs
+      putative_NA_counts = composition_NAs.collect { |key_value_array| key_value_array[1] }     # only count, not char
+      putative_NA_sum    = putative_NA_counts.inject { |sum, n| sum + n }                       # count of all putative NA
+      putative_NA_sum    = 0 if putative_NA_sum.nil?
+
+      if putative_NA_sum > (0.9 * cleaned_sequence.length)
+        return :nucleotide
+      else
+        return :protein
+      end
+    end
+
+    # splits input at putative fasta definition lines (like ">adsfadsf"), guesses sequence type for each sequence. 
+    # if not enough sequence to determine, returns nil.
+    # if 2 kinds of sequence mixed together, raises ArgumentError
+    # otherwise, returns :nucleotide or :protein
+    def type_of_sequences(fasta_format_string)
+      # the first sequence does not need to have a fasta definition line
+      sequences = fasta_format_string.split(/^>.*$/).delete_if { |seq| seq.empty? }
+
+      # get all sequence types
+      sequence_types = sequences.collect { |seq|  guess_sequence_type(seq) }.uniq.compact
+
+      return nil if sequence_types.empty?
+
+      if sequence_types.length == 1
+        return sequence_types.first # there is only one (but yes its an array)
+      else 
+        raise ArgumentError, "Insufficient info to determine sequence type. Cleaned queries are: #{ sequences.to_s }"
+      end
+    end
+
+    # Return the database type that can be used for a given blast method.
+    #   db_type_for("blastp")  => :protein
+    #   db_type_for("tblastn") => :nucleotide
+    def db_type_for(blast_method)
+      case blast_method
+      when 'blastp', 'blastx'
+        :protein
+      when 'blastn', 'tblastx', 'tblastn'
+        :nucleotide
+      end
+    end
+
+    # Return the blast methods that can be used for a given type of sequence.
+    #   blast_methods_for(:protein)     => ['blastp', 'tblastn']
+    #   blast_methods_for(:nucleotide)  => ['blastn','tblastx','blastx']
+    def blast_methods_for(seq_type)
+      case seq_type
+      when :protein
+        ['blastp', 'tblastn']
+      when :nucleotide
+        ['blastn','tblastx','blastx']
+      end
+    end
+
+    def sequence_from_blastdb(identifiers, db)  # helpful when displaying parsed blast results
+      entries_to_get = identifiers           if identifiers.class == String
+      entries_to_get = identifiers.join(',') if identifiers.class == Array
+      raise ArgumentError, "No ids to fetch: #{identifiers.to_s}" if entries_to_get.empty?
+
+      sequences = %x|blastdbcmd -db #{db} -entry #{entries_to_get} 2>&1|
+      if sequences.include?("No valid entries to search") 
+        raise ArgumentError, "Cannot find ids: #{entries_to_get} in #{db}." +
+        "OR makeblastdb needs to be rerun with '-parse_seqids'"
+      end
+
+      sequences.chomp + "\n"  # fastaformat in a string - not sure blastdbcmd includes newline
+    end
+
+  end
 end
