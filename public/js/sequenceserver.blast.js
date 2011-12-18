259be01ed7708aa69ceef06da7346af485d39b6f
Anurag Priyam <anurag08priyam@gmail.com>
2011-12-18 19:59:50 +0530

    SS.blast: add new events, `blast_valid` and `blast_invalid`
    Whenever the input sequence, or blast method, or database changes,
    SequenceServer determines if the current user input is sufficient to BLAST, and
    triggers `blast_valid` if it is, and `blast_invalid` if not.
    
    Signed-off-by: Anurag Priyam <anurag08priyam@gmail.com>

--- public/js/sequenceserver.blast.js
+++ public/js/sequenceserver.blast.js
@@ -15,10 +15,53 @@
         sequence_type_changed:
             When the type (nucleotide or protein) of the input sequence changes.
 
+        blast_valid:
+            If the current user input is sufficient to launch a BLAST
+
+        blast_invalid:
+            If the current user input is not sufficient to launch a BLAST
+
     TODO: should (probably) access UI objects through some sort of interface
 */
 SS.blast = (function () {
     /* private methods */
+
+    /*
+        check if blast is valid (sufficient input to blast or not)
+    */
+    var is_valid = function () {
+        // must enter a query
+        if (!$('#sequence').val()) {
+            return false;
+        }
+
+        // must select a blast method
+        if (!$('.blastmethods input:checked').val()) {
+            return false;
+        }
+
+        // must select atleast one database
+        if (!$('.databases input:checked').val()) {
+            return false;
+        }
+
+        // everything good
+        return true;
+    }
+
+    /*
+        signal blast's validity (sufficient input to blast or not)
+    */
+    var signal_blast_validity = function () {
+        $('#sequence, .blastmethods, .databases').change(function () {
+          if (is_valid()) {
+              $('form').trigger('blast_valid');
+          }
+          else {
+              $('form').trigger('blast_invalid');
+          }
+        });
+    }
 
     /*
         determine input sequence type, store it, and trigger
@@ -62,6 +105,7 @@ SS.blast = (function () {
 
     blast.init = function () {
         signal_sequence_type_changed();
+        signal_blast_validity();
     }
 
     return blast;
