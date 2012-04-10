da6d8e683807b042690567bf425f7f1510faaed9
Anurag Priyam <anurag08priyam@gmail.com>
2012-04-10 22:03:22 +0530

    popup users to plus-one or tweet about SequnceServer
    After they have used SS for a while.
    
    Signed-off-by: Anurag Priyam <anurag08priyam@gmail.com>

--- public/js/sequenceserver.js
+++ public/js/sequenceserver.js
@@ -302,4 +302,19 @@ $(document).ready(function(){
         on('remove.index', function () {
             $('#result').removeAttr('style');
         });
+
+    (function (store) {
+        try {
+            var visits = store.get('visits') || 0;
+            visits = visits + 1;
+            if (visits <= 10) {
+                store.set('visits', visits);
+
+                if (visits === 10) {
+                    $('#social').modal();
+                }
+            }
+        }
+        catch (e) {};
+    }(store));
 });
