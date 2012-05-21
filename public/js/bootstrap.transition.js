d94d62b0c1c9a12735615da0c87a71ec486fff9d
Anurag Priyam <anurag08priyam@gmail.com>
2012-05-21 13:25:29 +0530

    ui: slide in modal windows with Bootstrap's transition plugin
    Except for the one that displays spinner.  For some reason, the spinner doesn't
    align well if transition is enabled.
    
    Signed-off-by: Anurag Priyam <anurag08priyam@gmail.com>

---
+++ public/js/bootstrap.transition.js
@@ -0,0 +1,7 @@
+/**
+* Bootstrap.js by @fat & @mdo
+* plugins: bootstrap-transition.js
+* Copyright 2012 Twitter, Inc.
+* http://www.apache.org/licenses/LICENSE-2.0.txt
+*/
+!function(a){a(function(){a.support.transition=function(){var a=function(){var a=document.createElement("bootstrap"),b={WebkitTransition:"webkitTransitionEnd",MozTransition:"transitionend",OTransition:"oTransitionEnd",msTransition:"MSTransitionEnd",transition:"transitionend"},c;for(c in b)if(a.style[c]!==undefined)return b[c]}();return a&&{end:a}}()})}(window.jQuery)
