77dc46371a1123e503916d8b5d83efc130e06d36
Anurag Priyam <anurag08priyam@gmail.com>
2014-06-26 08:52:44 +0530

    Read Gemspec to find version number.
    Signed-off-by: Anurag Priyam <anurag08priyam@gmail.com>

--- lib/sequenceserver/version.rb
+++
@@ -1,9 +0,0 @@
-module SequenceServer
-
-  # Return version number of SequenceServer from the gemspec.  Version number
-  # is read from gemspec, which might fail on older versions of Bundler or
-  # RubyGems.  Return `nil` if version number couldn't be determined.
-  def self.version
-    Bundler.rubygems.loaded_specs('sequenceserver').version rescue nil
-  end
-end
