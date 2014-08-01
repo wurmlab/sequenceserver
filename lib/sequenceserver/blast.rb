cc4295a1af820af164258ab65c86d036a64fbc6a
Vivek Rai <vivekraiiitkgp@gmail.com>
2014-08-01 13:44:00 +0530

    Can handle more than one hsp per hit.
    * This patch fixes the earlier loophole which showed only one HSP per hit
      query.
    * A critical bug causing only one query to show up in case of
      multi query blast with similar ID's was fixed.
    * result.erb now incorporates the changes required to show multiple hsps
      per hit. However, a little formatting fixes might occur later.
    
    [ap: re-indent to two spaces instead of four]
    
    Signed-off-by: Vivek Rai <vivekraiiitkgp@gmail.com>
    Signed-off-by: Anurag Priyam <anurag08priyam@gmail.com>

--- lib/sequenceserver/blast.rb
+++ lib/sequenceserver/blast.rb
@@ -42,13 +42,44 @@ module SequenceServer
     # @member [String]     accession
     # @member [Fixnum]     len
     # @member [HSP]        hsp
-    Hit = Struct.new(:number, :id, :def, :accession, :len, :hsp)
+    Hit = Struct.new(:number, :id, :def, :accession, :len, :hsps) do
+      def initialize(*args)
+        super(*args)
+        self.number = args[0].to_i
+        self.id = args[1]
+        self.def = args[2]
+        self.accession = args[3]
+        self.len = args[4].to_i
+        self.hsps = args[5]
+      end
+    end
 
     # Structure to hold the HSP information about each hit.
     # More information about values can be found at NCBI's BLAST manual page.
     HSP = Struct.new(:number, :bit_score, :score, :evalue, :qstart, :qend, :start, \
                      :send, :qframe, :hframe, :identity, :positives, :gaps, :len, \
-                     :qseq, :hseq, :midline)
+                     :qseq, :hseq, :midline) do
+      def initialize(*args)
+        super(*args)
+        self.number = args[0].to_i
+        self.bit_score = args[1].to_f.round(2)
+        self.score = args[2].to_i
+        self.evalue = args[3]
+        self.qstart = args[4].to_i
+        self.qend = args[5].to_i
+        self.start = args[6].to_i
+        self.send = args[7].to_i
+        self.qframe = args[8].to_i
+        self.hframe = args[9].to_i
+        self.identity = args[10].to_i
+        self.positives = args[11].to_i
+        self.gaps = args[12].to_i
+        self.len = args[13].to_i
+        self.qseq = args[14]
+        self.hseq = args[15]
+        self.midline = args[16]
+      end
+    end
 
     def initialize(method, query, databases, options = nil)
       @method    = method
@@ -95,19 +126,21 @@ module SequenceServer
       @program = hashed_out["BlastOutput_program"]
       @querydb = hashed_out["BlastOutput_db"]
 
-      hashed_out["BlastOutput_iterations"].each do |n|
-        @queries ||= {}
-        @queries[n[2]] = Query.new(n[0], n[2], n[3], {}, n[5]["Statistics"])
+      hashed_out["BlastOutput_iterations"].each_with_index do |n, i|
+        @queries ||= []
+        @queries.push(Query.new(n[0], n[2], n[3], [], n[5]["Statistics"]))
 
         # Ensure a hit object is received. No hits, returns a newline.
         # Note that checking to "\n" doesn't work since n[4] = ["\n"]
         if n[4]==["\n"]
-          @queries[n[2]][:hits] = []
+          @queries[n[1]][:hits] = []
         else
-          n[4].each do |hits|
-            @queries[n[2]][:hits][hits[1]] = Hit.new(hits[0], hits[1], hits[2],\
-                                                     hits[3], hits[4], {})
-            @queries[n[2]][:hits][hits[1]][:hsp] = HSP.new(*hits[5]["Hsp"].values)
+          n[4].each_with_index do |hits, j|
+            @queries[i][:hits].push(Hit.new(hits[0], hits[1], hits[2],\
+                                                     hits[3], hits[4], []))
+            hits[5].each_with_index do |hsp, k|
+              @queries[i][:hits][j][:hsps].push(HSP.new(*hits[5][k].values))
+            end
           end
         end
       end
@@ -152,11 +185,11 @@ module SequenceServer
       if node.is_a?(String)
         return node
       end
-      if ['Parameters', 'Hit_hsps', 'BlastOutput_param', 'Iteration_stat', \
-          'Hsp', 'Statistics'].include? node.name
+      if ['Parameters', 'BlastOutput_param', 'Iteration_stat', \
+          'Statistics', 'Hsp'].include? node.name
         value = node_to_dict(node)
-      elsif ['Hit', 'Iteration_hits', 'BlastOutput_iterations', \
-             'Iteration'].include? node.name
+      elsif ['Iteration_hits', 'BlastOutput_iterations', \
+             'Iteration', 'Hit', 'Hit_hsps'].include? node.name
         value = node_to_array(node)
       else
         value = first_text(node)
