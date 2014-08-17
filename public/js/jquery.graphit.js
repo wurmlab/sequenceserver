ec5c7f83424268f0517fb41fb5ea56d0c6e358c7
Vivek Rai <vivekraiiitkgp@gmail.com>
2014-08-17 20:00:23 +0530

    Connect hsps by line, remove arrow heads
    The arrow heads have been removed as they don't make much sense for all
    kind of blast types (NCBI too doesn't do arrows). Added connecting
    lines between hsps of single hit and reduced margin around the graph to
    appear aligned along the container.
    The barwidth was also decreased for more sleekier look.
    
    Signed-off-by: Vivek Rai <vivekraiiitkgp@gmail.com>

--- public/js/jquery.graphit.js
+++ public/js/jquery.graphit.js
@@ -2,7 +2,7 @@
     $.extend({
         draw: function() {
             var margin = 20;
-            var barheight = 8, barpadding = 5, legend = 10;
+            var barheight = 5, barpadding = 5, legend = 10;
 
             $("[data-graphit='overview']").each( function(i) {
                 var q_i = i+1;
@@ -31,22 +31,25 @@
                 // console.log(hits);
 
                 query_len = $(this).data().queryLen;
-                width = $(this).width();
-                height = hits.length*(barheight + barpadding) + 5*margin + legend*3;
                 var graph_dom = $(this).children().eq(1).children().eq(0).after("<div class='graph'/>");
+                width = $('.graph', this).width();
+                height = hits.length*(barheight + barpadding) + 5*margin + legend*3;
 
                 var svg = d3.select($(this).find('.graph')[0])
                             .data([hits])
                             .append('svg')
                             .attr('width', width)
                             .attr('height', height).append('g')
-                            .attr('transform', 'translate('+margin+', '+margin+')');
+                            .attr('transform', 'translate('+margin/4+', '+margin/4+')');
                 // DEBUG
                 // console.log(svg);
 
                 var x = d3.scale.linear().range([0, width-margin*2])
                 x.domain([0, query_len]);
-                var xAxis = d3.svg.axis().scale(x).orient('top').ticks(10);
+                var xAxis = d3.svg.axis()
+                    .scale(x)
+                    .orient('top')
+                    .ticks(10);
 
                 // Attach the axis to DOM (<svg> element)
                 var scale = svg.append('g')
@@ -75,35 +78,56 @@
                     //})
                     .each( function(d,i) {
                         var h_i = i+1;
-                        //console.log(i);
                         /*
                         var color_hsp = d3.scale.ordinal()
                                         .domain((d.map( function(d) { return d.hspId; } )))
                                         .range(["blue", "red", "green", "yellow", "orange"]);
                         */
-                        //console.log(d);
+                        var p_hsp = d;
+                        var p_id = d.hitId;
+                        var p_count = d.length;
 
                         d3.select(this)
                             .selectAll('.hsps')
                             .data(d).enter()
                             .append('a')
                         .each( function(pd, j) {
-                            var p_id = d3.select(this.parentNode).datum().hitId;
-                            //console.log(index);
                             //console.log(d3.select(this.parentNode).datum().hitId);
+                            //
+                            var y_hspline = y(p_id)+barheight/2;
+                            var hspline_color = d3.rgb(20,20,20);
+
+                            if (j+1 < p_count) {
+                                if( p_hsp[j].hspEnd < p_hsp[j+1].hspStart ) {
+                                    d3.select(this.parentNode).append('line')
+                                        .attr('x1', x(p_hsp[j].hspEnd))
+                                        .attr('y1', y_hspline)
+                                        .attr('x2', x(p_hsp[j+1].hspStart))
+                                        .attr('y2', y_hspline)
+                                        .attr('stroke', hspline_color);
+                                }
+                                else if ( p_hsp[j].hspStart > p_hsp[j+1].hspEnd ) {
+                                    d3.select(this.parentNode).append('line')
+                                        .attr('x1', x(p_hsp[j+1].hspEnd))
+                                        .attr('y1', y_hspline)
+                                        .attr('x2', x(p_hsp[j].hspStart))
+                                        .attr('y2', y_hspline)
+                                        .attr('stroke', hspline_color);
+                                };
+                            };
 
                             d3.select(this)//.append('a')
                             .attr('xlink:href', function(d,i) {return '#Query_'+(q_i)+'_hit_'+(h_i);})
                             .append('rect')
                             .attr('x', function(d) {
                                 if(d.hspFrame < 0)
-                                    return x(d.hspStart)+3;
+                                    return x(d.hspStart);
                                 else
                                     return x(d.hspStart);
                             })
                             .attr('y', y(p_id))
                             .attr('width', function(d) {
-                                    return x(d.hspEnd - d.hspStart)-3;
+                                    return x(d.hspEnd - d.hspStart);
                             })
                             .attr('height', barheight)
                             .attr('fill', d3.rgb(color2(p_id),color2(p_id),color2(p_id)))
@@ -111,17 +135,6 @@
                                 //return d3.rgb(0,0,color_hsp(d.hspId));
                                 return d3.rgb(0, 0, 255-y(d.hitId));
                             }) */
-                            d3.select(this).append('path')
-                            .attr('d', d3.svg.symbol().type('triangle-up').size(35))
-                            //.attr('fill', 'green')
-                            .attr('fill', d3.rgb(color2(p_id),color2(p_id),color2(p_id)))
-                            .attr('transform', function(d, i) {
-                                // console.log(d.hitFrame);
-                                if(d.hspFrame > 0)
-                                    return ('translate('+(x(d.hspEnd)+1)+', '+(y(p_id)+4)+')rotate(90)');
-                                else
-                                    return ('translate('+(x(d.hspStart)-1)+', '+(y(p_id)+4)+')rotate(-90)');
-                            });
                         });
                     });
                 //console.log(t);
@@ -138,18 +151,19 @@
                 svg_legend.append('text')
                         .attr('transform', 'translate(0, '+legend+')')
                         .attr('x', 6*(width-2*margin)/10 - margin/2)
-                        .text("Low evalue");
+                        .text("Weak hits");
                 svg_legend.append('text')
                         .attr('transform', 'translate(0, '+legend+')')
                         .attr('x', 9*(width-2*margin)/10 + margin/2)
-                        .text("High evalue");
+                        .text("Strong hits");
 
                 svg.append('linearGradient')
                     .attr('id', 'legend-grad')
                   .selectAll('stop')
                     .data([
-                        {offset: "0%", color: "#eee"},
-                        {offset: "100%", color: "#333"}
+                        {offset: "0%", color: "#ccc"},
+                        {offset: '50%', color: '#888'},
+                        {offset: "100%", color: "#000"}
                         ])
                   .enter().append('stop')
                     .attr('offset', function(d) { return d.offset })
