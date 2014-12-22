170c63fff0d14ef8eac1f5de8f921edbd8b05ddf
Vivek Rai <vivekraiiitkgp@gmail.com>
2014-12-22 21:03:22 +0530

    Add a headless unit test doing a simple BLAST search.
    The basic framework is set with a simple test which performs a BLAST
    query with SequenceServer.
    
    Signed-off-by: Vivek Rai <vivekraiiitkgp@gmail.com>

--- spec/capybara_spec.rb
+++ spec/capybara_spec.rb
@@ -4,21 +4,20 @@ require 'capybara-webkit'
 
 describe "an headless mode with webkit" do
   sequence = "ATCGATCAGCTACGATCAGCATCGACTAGCATCGACTACGA"
+  sample_nucl_db = "Sinvicta2-2-3.cdna"
+  sample_prot_db = "Sinvicta2-2-3.prot"
 
   before(:all) do
-    Capybara.use_default_driver
-    Capybara.javascript_driver = :webkit
-    Capybara.app = SequenceServer.init
+    Capybara.current_driver = :webkit
+    Capybara.app = SequenceServer
   end
 
   it 'runs a simple blast' do
     visit '/'
     fill_in('sequence', :with => sequence)
-    within('ul.nucleotide') do
-      check(find("input[type='checkbox']"))
-    end
-    click_button('submit')
-    save_and_open_page
+    check(sample_nucl_db)
+    click_button('method')
+    page.should have_content('Query')
   end
 
 end
