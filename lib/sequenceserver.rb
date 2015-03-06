80f42e616110310dca4e996370ae91daf512fb1e
IsmailM <ismail.moghul@gmail.com>
2015-03-06 15:46:31 +0000

    use Rack::CommonLogger only in the development environment
    Signed-off-by: IsmailM <ismail.moghul@gmail.com>

--- lib/sequenceserver.rb
+++ lib/sequenceserver.rb
@@ -55,7 +55,7 @@ module SequenceServer
       server = Thin::Server.new(config[:host],
                                 config[:port],
                                 :signals => false) do
-        use Rack::CommonLogger
+        use Rack::CommonLogger if SequenceServer.environment == 'development'
         run SequenceServer
       end
       server.silent = true
