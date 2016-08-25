0c7e746629f687030da4418e346137c802e32c48
Anurag Priyam <anurag08priyam@gmail.com>
2016-08-25 10:13:21 +0530

    Refine grapher's header.
    And refactor svg.css a bit.
    
    Signed-off-by: Anurag Priyam <anurag08priyam@gmail.com>

--- public/js/grapher.js
+++ public/js/grapher.js
@@ -34,28 +34,30 @@ export default function Grapher(Graph) {
 
         render () {
             return (
-                <div className="grapher" ref="grapher">
-                    <div className='container-fluid'>
-                      <h5
-                          className="caption"
-                          data-toggle="collapse"
-                          data-target={"#Collapse_"+Graph.collapseId(this.props)}>
-                          <i className="fa fa-chevron-down"></i>
-                          &nbsp;
-                          {Graph.name()}
-                      </h5>
-                      <div
-                          className="graph-links hit-links">
-                          <a href = "#" className="export-to-svg">
-                              <i className="fa fa-download"/>
-                              <span>{"  SVG  "}</span>
-                          </a>
-                          <span>{" | "}</span>
-                          <a href = "#" className="export-to-png">
-                              <i className="fa fa-download"/>
-                              <span>{"  PNG  "}</span>
-                          </a>
-                      </div>
+                <div
+                    className="grapher" ref="grapher">
+                    <div
+                        className="grapher-header">
+                        <h5
+                            className="caption"
+                            data-toggle="collapse"
+                            data-target={"#Collapse_"+Graph.collapseId(this.props)}>
+                            <i className="fa fa-chevron-down"></i>
+                            &nbsp;
+                            {Graph.name()}
+                        </h5>
+                        <div
+                            className="hit-links graph-links">
+                            <a href = "#" className="export-to-svg">
+                                <i className="fa fa-download"/>
+                                <span>{"  SVG  "}</span>
+                            </a>
+                            <span>{" | "}</span>
+                            <a href = "#" className="export-to-png">
+                                <i className="fa fa-download"/>
+                                <span>{"  PNG  "}</span>
+                            </a>
+                        </div>
                     </div>
                     <div ref="svgContainer" id={"Collapse_"+Graph.collapseId(this.props)}
                         className={"svg-container " + Graph.className()}>
