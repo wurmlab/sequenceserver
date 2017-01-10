a42af42d9f70d2117e47518e28d01aac078066c7
Lawrence <enjoyyourcopepods@gmx.com>
2017-01-10 19:26:34 +0000

    Revert "Refactoring in the 'sys' method"
    This reverts commit b8f6a6b62a52ffa05cff801c86653d48745de1f3.

--- lib/sequenceserver.rb
+++ lib/sequenceserver.rb
@@ -93,7 +93,8 @@ module SequenceServer
       child_pid = fork do
         # Set the PATH environment variable to the binary directory or
         # safe directory.
-        ENV['PATH'] = config[:bin] || options[:path] || ENV['PATH']
+        ENV['PATH'] = config[:bin] if config[:bin] && !options[:path]
+        ENV['PATH'] = options[:path] if options[:path]
 
         # Change to the specified directory.
         Dir.chdir(options[:dir]) if options[:dir] && Dir.exist?(options[:dir])
