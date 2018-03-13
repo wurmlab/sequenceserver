251df1477bbfd6e216d231ee0e3f415c9ddbbf39
Anurag Priyam <anurag08priyam@gmail.com>
2018-03-13 20:19:46 +0000

    Refactor sequenceserver_spec further
    Signed-off-by: Anurag Priyam <anurag08priyam@gmail.com>

--- spec/sequenceserver_spec.rb
+++ spec/sequenceserver_spec.rb
@@ -2,65 +2,44 @@ require 'spec_helper'
 
 # Test the main module, startup process, etc.
 module SequenceServer
-  # NOTE:
-  #   Testing SequenceServer's initialization can be a bit tricky. For
-  #   instance, to test num_threads, correct database_dir must be set.
   describe 'SequenceServer' do
-    let 'root' do
-      SequenceServer.root
-    end
 
-    let 'empty_config' do
-      File.join(root, 'spec', 'empty_config.yml')
-    end
-
-    let 'database_dir' do
-      File.join(root, 'spec', 'database', 'sample')
-    end
-
-    let 'database_dir_no_db' do
-      File.join(root, 'spec', 'database', 'unformatted',
-                'Cardiocondyla_obscurior')
-    end
-
-    let 'config' do
-      { :config_file => empty_config }
-    end
-
+    # Clear config and databases before each test.
     before :each do
       @config = nil
+      Database.clear
     end
 
     # bin, if set, should be a _directory_ that exists
     it 'raises appropriate error if bin incorrectly set' do
-      # Raise if bin dir does not exist.
-      expect do
-        SequenceServer.init(config.update :bin => '/foo/bar')
-      end.to raise_error(ENOENT)
-
       # Raise if bin dir is not a directory.
       expect do
-        SequenceServer.init(config.update :bin => __FILE__)
+        SequenceServer.init(bin: __FILE__)
+      end.to raise_error(ENOENT)
+
+      # Raise if bin dir does not exist.
+      expect do
+        SequenceServer.init(bin: '/foo/bar')
       end.to raise_error(ENOENT)
     end
 
     # database_dir is compulsory
     it 'raises appropriate error if database_dir not set' do
       expect do
-        SequenceServer.init(config.update :config_file => empty_config)
+        SequenceServer.init(database_dir: nil)
       end.to raise_error(DATABASE_DIR_NOT_SET)
     end
 
     # database_dir, when set, should be a _directory_ that exists.
     it 'raises appropriate error if database_dir incorrectly set' do
-      # Raise if database_dir does not exist.
-      expect do
-        SequenceServer.init(config.update :database_dir => '/foo/bar')
-      end.to raise_error(ENOENT)
-
       # Raise if database_dir is not a directory.
       expect do
-        SequenceServer.init(config.update :database_dir => __FILE__)
+        SequenceServer.init(database_dir: __FILE__)
+      end.to raise_error(ENOENT)
+
+      # Raise if database_dir does not exist.
+      expect do
+        SequenceServer.init(database_dir: '/foo/bar')
       end.to raise_error(ENOENT)
     end
 
@@ -69,7 +48,9 @@ module SequenceServer
     it "raises appropriate error if database_dir doesn't contain any BLAST+"\
        'database' do
       expect do
-        SequenceServer.init(config.update :database_dir => database_dir_no_db)
+        database_dir_no_db = File.join(__dir__, 'database', 'unformatted',
+                                       'Cardiocondyla_obscurior')
+        SequenceServer.init(database_dir: database_dir_no_db)
       end.to raise_error(NO_BLAST_DATABASE_FOUND)
     end
 
@@ -77,14 +58,12 @@ module SequenceServer
     it 'raises appropriate error if num_threads incorrectly set' do
       # Raise if not a number.
       expect do
-        SequenceServer.init(config.update :database_dir => database_dir,
-                                          :num_threads  => 'foo')
+        SequenceServer.init(num_threads: 'foo')
       end.to raise_error(NUM_THREADS_INCORRECT)
 
       # Raise if less than 1.
       expect do
-        SequenceServer.init(config.update :database_dir => database_dir,
-                                          :num_threads  => 0)
+        SequenceServer.init(num_threads: 0)
       end.to raise_error(NUM_THREADS_INCORRECT)
     end
 
@@ -92,22 +71,20 @@ module SequenceServer
     it 'raises appropriate error if require incorrectly set' do
       # Raise if not found.
       expect do
-        SequenceServer.init(config.update :database_dir => database_dir,
-                                          :require      => 'foo/bar')
+        SequenceServer.init(require: 'foo/bar')
       end.to raise_error(ENOENT)
 
       # Raise if directory.
       expect do
-        SequenceServer.init(config.update :database_dir => database_dir,
-                                          :require => File.dirname(__FILE__))
+        SequenceServer.init(require: __dir__)
       end.to raise_error(ENOENT)
     end
 
     it 'has a list of databases after startup' do
-      SequenceServer::Database.clear
-      SequenceServer.init(config.update :database_dir => database_dir)
-      Database.all.should_not be_empty
+      SequenceServer.init()
+      # assuming database_dir is set to spec/database/sample
       Database.all.length.should == 2
+      Database.all.should_not be_empty
     end
   end
 end
