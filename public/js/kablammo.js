634aa6241adcd395213b6cb7013166cee5a4d768
Anurag Priyam <anurag08priyam@gmail.com>
2018-06-18 15:37:52 +0100

    Remove border from hsps and hsps overview
    Accordingly adjust vertical and left padding for hits.
    
    Signed-off-by: Anurag Priyam <anurag08priyam@gmail.com>

--- public/js/kablammo.js
+++ public/js/kablammo.js
@@ -36,7 +36,7 @@ class Graph {
 
   constructor($svgContainer, props) {
     this._zoom_scale_by = 1.4;
-    this._padding_x = 17.5;
+    this._padding_x = 12;
     this._padding_y = 50;
 
     this._canvas_height = $svgContainer.height();
