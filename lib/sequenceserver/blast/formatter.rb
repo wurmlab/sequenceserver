19d9e24cc766d007f464cd714fe223dd3c1bc771
Anurag Priyam <anurag08priyam@gmail.com>
2018-06-29 11:00:11 +0100

    Use sys's stdout option in BLAST::Formatter
    Instead of blast_formatter's -out option. Just being consistent
    - we don't use -out option while running the search either.
    
    Increase documentation comments and fix funny line breaks.
    
    Signed-off-by: Anurag Priyam <anurag08priyam@gmail.com>

--- lib/sequenceserver/blast/formatter.rb
+++ lib/sequenceserver/blast/formatter.rb
@@ -2,24 +2,26 @@ require 'forwardable'
 
 module SequenceServer
   module BLAST
-    # Formats BLAST+ archive file format into other file formats.
+    # Formatter is invoked during report generation or for results download to
+    # convert BLAST+ archive file to other formats. Formatter generates output
+    # in Job#dir. Output files persist till the job itself is deleted. Calling
+    # Formatter a second time (for the same input job and output format) will
+    # return saved ouput.
     class Formatter
       class << self
         alias_method :run, :new
       end
 
       extend Forwardable
-      def_delegators SequenceServer, :config, :logger, :sys
+      def_delegators SequenceServer, :config, :sys
 
       def initialize(job, type)
         @job = job
-        @format, @mime, @specifiers = OUTFMT[type]
         @type = type
 
+        @format, @mime, @specifiers = OUTFMT[type]
         run
       end
-
-      attr_reader :job, :type
 
       attr_reader :format, :mime, :specifiers
 
@@ -28,20 +30,23 @@ module SequenceServer
       end
 
       def filename
-        @filename ||=
-          "sequenceserver-#{type}_report.#{mime}"
+        @filename ||= "sequenceserver-#{type}_report.#{mime}"
       end
 
       private
 
+      attr_reader :job, :type
+
       def run
         return if File.exist?(file)
-        command =
-          "blast_formatter -archive '#{job.stdout}'" \
-          " -outfmt '#{format} #{specifiers}'" \
-          " -out '#{file}'"
-        sys(command, path: config[:bin], dir: DOTDIR)
+        command = "blast_formatter -archive '#{job.stdout}'" \
+          " -outfmt '#{format} #{specifiers}'"
+        sys(command, path: config[:bin], dir: DOTDIR,
+            stdout: file)
       rescue CommandFailed => e
+        # Mostly we will never get here: empty archive file,
+        # file permissions, broken BLAST binaries, etc. will
+        # have been caught before reaching here.
         fail SystemError, e.stderr
       end
     end
