b5b8aeb91ffa24fffc56ebf6537620bb058e6da1
Anurag Priyam <anurag08priyam@gmail.com>
2018-09-10 09:45:56 +0100

    Code style improvements
    [ci skip]
    
    Signed-off-by: Anurag Priyam <anurag08priyam@gmail.com>

--- lib/sequenceserver/blast/query.rb
+++ lib/sequenceserver/blast/query.rb
@@ -10,7 +10,7 @@ module SequenceServer
       def initialize(*args)
         args[1] = args[1].to_i
         if args[2] == 'No definition line' ||
-            args[2] == 'unnamed protein product'
+           args[2] == 'unnamed protein product'
           args[2] = "Query_#{args[1]}"
         end
         args[3] = args[3].to_i
@@ -22,10 +22,10 @@ module SequenceServer
       attr_reader :id, :title
 
       def to_json(*args)
-        %i[number id title length hits].inject({}) { |h, k|
+        %i[number id title length hits].inject({}) do |h, k|
           h[k] = send(k)
           h
-        }.to_json(*args)
+        end.to_json(*args)
       end
     end
   end
