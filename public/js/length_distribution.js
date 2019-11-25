e1e683ab4069f7e9877bf1a1215940eee27efc1c
Iwo Pieniak <ivo.pieniak@gmail.com>
2019-11-25 18:10:23 +0000

    Length distribution issue #409
    The scale of the query and the hits is different when BLASTX is
    selected. As the query is a translated nucleotide sequence, it should be
    divided by 3 when presented with the hits which are proteins.
    
    Following solution detects the algorithm in the constructor and applies
    correct query.length making the visualisation correct.
    
    Signed-off-by: Iwo Pieniak <ivo.pieniak@gmail.com>

--- public/js/length_distribution.js
+++ public/js/length_distribution.js
@@ -28,7 +28,10 @@ class Graph {
         this.query = props.query;
         this._seq_type = Helpers.get_seq_type(props.algorithm);
         this.svg_container = $svg_container;
-
+        this.query_length = this.query.length;
+        if (props.algorithm == 'blastx') {
+            this.query_length = this.query.length / 3;
+        }
         this._margin = {top: 30, right: 25, bottom: 55, left: 12.5};
         this.initiate($svg_container.width(), $svg_container.height());
     }
