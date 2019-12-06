35a713a439c20d0b20ee21fb07ad2f3e1cfd5bee
Iwo Pieniak <ivo.pieniak@gmail.com>
2019-12-06 14:37:20 +0000

    Eslint formatting for utils.js
    Signed-off-by: Iwo Pieniak <ivo.pieniak@gmail.com>

--- public/js/utils.js
+++ public/js/utils.js
@@ -10,8 +10,8 @@ var Utils = {
         {
             return (
                 <a href={link.url} className={link.class} target='_blank'>
-                    {link.icon && <i className={"fa " + link.icon}></i>}
-                    {" " + link.title + " "}
+                    {link.icon && <i className={'fa ' + link.icon}></i>}
+                    {' ' + link.title + ' '}
                 </a>
             );
         }
@@ -24,7 +24,7 @@ var Utils = {
 
     // Formats an array of two elements as "first (last)".
     format_2_tuple: function (tuple) {
-        return (tuple[0] + " (" + tuple[tuple.length - 1] + ")");
+        return (tuple[0] + ' (' + tuple[tuple.length - 1] + ')');
     },
 
     /**
@@ -38,14 +38,14 @@ var Utils = {
      * Returns fractional representation as String.
      */
     inFraction: function (num , den) {
-        return num + "/" + den;
+        return num + '/' + den;
     },
 
     /**
      * Returns given Float as String formatted to two decimal places.
      */
     inTwoDecimal: function (num) {
-        return num.toFixed(2)
+        return num.toFixed(2);
     },
 
     /**
@@ -55,19 +55,19 @@ var Utils = {
     inExponential: function (num) {
         // Nothing to do if num is 0.
         if (num === 0) {
-            return 0
+            return 0;
         }
 
         // Round to two decimal places if in the rane [1..10).
         if (num >= 1 && num < 10)
         {
-            return this.inTwoDecimal(num)
+            return this.inTwoDecimal(num);
         }
 
         // Return numbers in the range [0..1) and [10..Inf] in
         // scientific format.
         var exp = num.toExponential(2);
-        var parts = exp.split("e");
+        var parts = exp.split('e');
         var base  = parts[0];
         var power = parts[1];
         return <span>{base} &times; 10<sup>{power}</sup></span>;
