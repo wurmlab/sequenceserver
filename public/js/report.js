bc40a6b7273b78beb612ab245b2ab6c16c07f047
Iwo Pieniak <ivo.pieniak@gmail.com>
2019-12-09 13:44:10 +0000

    ESlint report.js cleanup
    Signed-off-by: Iwo Pieniak <ivo.pieniak@gmail.com>

--- public/js/report.js
+++ public/js/report.js
@@ -1,4 +1,4 @@
-import './sequenceserver' // for custom $.tooltip function
+import './sequenceserver'; // for custom $.tooltip function
 import React from 'react';
 import _ from 'underscore';
 
@@ -19,8 +19,8 @@ import showErrorModal from './error_modal';
  */
 var downloadFASTA = function (sequence_ids, database_ids) {
     var form = $('<form/>').attr('method', 'post').attr('action', 'get_sequence');
-    addField("sequence_ids", sequence_ids);
-    addField("database_ids", database_ids);
+    addField('sequence_ids', sequence_ids);
+    addField('database_ids', database_ids);
     form.appendTo('body').submit().remove();
 
     function addField(name, val) {
@@ -28,7 +28,7 @@ var downloadFASTA = function (sequence_ids, database_ids) {
             $('<input>').attr('type', 'hidden').attr('name', name).val(val)
         );
     }
-}
+};
 
 /**
  * Base component of report page. This component is later rendered into page's
@@ -317,7 +317,7 @@ var Report = React.createClass({
      * Prevents folding of hits during text-selection.
      */
     preventCollapseOnSelection: function () {
-        $('body').on('mousedown', ".hit > .section-header > h4", function (event) {
+        $('body').on('mousedown', '.hit > .section-header > h4', function (event) {
             var $this = $(this);
             $this.on('mouseup mousemove', function handler(event) {
                 if (event.type === 'mouseup') {
@@ -363,7 +363,7 @@ var Report = React.createClass({
      */
     selectHit: function (id) {
 
-        var checkbox = $("#" + id);
+        var checkbox = $('#' + id);
         var num_checked  = $('.hit-links :checkbox:checked').length;
 
         if (!checkbox || !checkbox.val()) {
@@ -374,7 +374,7 @@ var Report = React.createClass({
 
         // Highlight selected hit and enable 'Download FASTA/Alignment of
         // selected' links.
-        if (checkbox.is(":checked")) {
+        if (checkbox.is(':checked')) {
             $hit.find('.section-content').addClass('glow');
             $('.download-alignment-of-selected').enable();
             $('.download-fasta-of-selected').enable();
@@ -414,7 +414,7 @@ var Query = React.createClass({
      * Returns the id of query.
      */
     domID: function () {
-        return "Query_" + this.props.query.number;
+        return 'Query_' + this.props.query.number;
     },
 
     queryLength: function () {
@@ -500,9 +500,9 @@ var HitsTable = React.createClass({
     mixins: [Utils],
     render: function () {
         var count = 0,
-          hasName = _.every(this.props.query.hits, function(hit) {
-            return hit.sciname !== '';
-          });
+            hasName = _.every(this.props.query.hits, function(hit) {
+                return hit.sciname !== '';
+            });
 
         return (
             <table
@@ -524,9 +524,9 @@ var HitsTable = React.createClass({
                         _.map(this.props.query.hits, _.bind(function (hit) {
                             return (
                                 <tr key={hit.number}>
-                                    <td className="text-left">{hit.number + "."}</td>
+                                    <td className="text-left">{hit.number + '.'}</td>
                                     <td>
-                                        <a href={"#Query_" + this.props.query.number + "_hit_" + hit.number}>
+                                        <a href={'#Query_' + this.props.query.number + '_hit_' + hit.number}>
                                             {hit.id}
                                         </a>
                                     </td>
@@ -536,7 +536,7 @@ var HitsTable = React.createClass({
                                     <td className="text-right">{this.inExponential(hit.hsps[0].evalue)}</td>
                                     <td className="text-right">{hit.identity}</td>
                                 </tr>
-                            )
+                            );
                         }, this))
                     }
                 </tbody>
@@ -571,7 +571,7 @@ var Hit = React.createClass({
      * Returns id that will be used for the DOM node corresponding to the hit.
      */
     domID: function () {
-        return "Query_" + this.props.query.number + "_hit_" + this.props.hit.number;
+        return 'Query_' + this.props.query.number + '_hit_' + this.props.hit.number;
     },
 
     databaseIDs: function () {
@@ -603,10 +603,10 @@ var Hit = React.createClass({
             hsp.query_id = this.props.query.id;
             hsp.hit_id = this.props.hit.id;
             return hsp;
-        }, this))
+        }, this));
 
         var aln_exporter = new AlignmentExporter();
-        aln_exporter.export_alignments(hsps, this.props.query.id+"_"+this.props.hit.id);
+        aln_exporter.export_alignments(hsps, this.props.query.id+'_'+this.props.hit.id);
     },
 
 
@@ -688,9 +688,9 @@ var Hit = React.createClass({
         return (
             <div className="hit-links">
                 <label>
-                    <input type="checkbox" id={this.domID() + "_checkbox"}
+                    <input type="checkbox" id={this.domID() + '_checkbox'}
                         value={this.accession()} onChange={function () {
-                            this.props.selectHit(this.domID() + "_checkbox");
+                            this.props.selectHit(this.domID() + '_checkbox');
                         }.bind(this)} data-target={'#' + this.domID()}
                     /> Select
                 </label>
@@ -732,10 +732,10 @@ var Hit = React.createClass({
                     return <HSP key={hsp.number}
                         algorithm={this.props.algorithm}
                         queryNumber={this.props.query.number}
-                        hitNumber={this.props.hit.number} hsp={hsp}/>
+                        hitNumber={this.props.hit.number} hsp={hsp}/>;
                 }, this)
             }
-        </div>
+        </div>;
     }
 });
 
@@ -904,7 +904,7 @@ var SequenceViewer = (function () {
                         sequences: response.sequences,
                         error_msgs: response.error_msgs,
                         requestCompleted: true
-                    })
+                    });
                 }, this))
                 .fail(function (jqXHR, status, error) {
                     showErrorModal(jqXHR, function () {
@@ -959,11 +959,11 @@ var SideBar = React.createClass({
                     hsp.hit_id = hit.id;
                     hsp.query_id = query.id;
                     hsps_arr.push(hsp);
-                })
-            })
+                });
+            });
         }, this));
         console.log('len '+hsps_arr.length);
-        aln_exporter.export_alignments(hsps_arr, "alignment-"+sequence_ids.length+"_hits");
+        aln_exporter.export_alignments(hsps_arr, 'alignment-'+sequence_ids.length+'_hits');
         return false;
     },
 
@@ -985,7 +985,7 @@ var SideBar = React.createClass({
                 }
             });
         }, this));
-        aln_exporter.export_alignments(hsps_arr, "alignment-"+sequence_ids.length+"_hits");
+        aln_exporter.export_alignments(hsps_arr, 'alignment-'+sequence_ids.length+'_hits');
         return false;
     },
 
@@ -997,29 +997,29 @@ var SideBar = React.createClass({
                 { this.props.shouldShowIndex && this.index() }
                 { this.downloads() }
             </div>
-        )
+        );
     },
 
     index: function () {
         return (
             <div className="index">
                 <div
-                  className="section-header">
-                  <h4>
-                      { this.summary() }
-                  </h4>
+                    className="section-header">
+                    <h4>
+                        { this.summary() }
+                    </h4>
                 </div>
                 <ul
                     className="nav hover-reset active-bold">
                     {
                         _.map(this.props.data.queries, _.bind(function (query) {
                             return (
-                                <li key={"Side_bar_"+query.id}>
+                                <li key={'Side_bar_'+query.id}>
                                     <a
                                         className="nowrap-ellipsis hover-bold"
-                                        href={"#Query_" + query.number}
-                                        title={"Query= " + query.id + ' ' + query.title}>
-                                        {"Query= " + query.id}
+                                        href={'#Query_' + query.number}
+                                        title={'Query= ' + query.id + ' ' + query.title}>
+                                        {'Query= ' + query.id}
                                     </a>
                                 </li>
                             );
@@ -1037,7 +1037,7 @@ var SideBar = React.createClass({
 
         return (
             program.toUpperCase() + ': ' +
-            numqueries + ' ' + (numqueries > 1 ? 'queries' : 'query') + ", " +
+            numqueries + ' ' + (numqueries > 1 ? 'queries' : 'query') + ', ' +
             numquerydb + ' ' + (numquerydb > 1 ? 'databases' : 'database')
         );
     },
@@ -1086,7 +1086,7 @@ var SideBar = React.createClass({
                                 name, alignment length, mismatches, gaps, identity,
                                 start and end coordinates, e value, bitscore, query
                                 coverage per subject and per HSP."
-                                href={"download/" + this.props.data.search_id + ".std_tsv"}>
+                                href={'download/' + this.props.data.search_id + '.std_tsv'}>
                                 Standard tabular report
                             </a>
                         </li>
@@ -1098,7 +1098,7 @@ var SideBar = React.createClass({
                                 accessions, and length; alignment details;
                                 taxonomy details of subject sequence(s) and
                                 query coverage per subject and per HSP."
-                                href={"download/" + this.props.data.search_id + ".full_tsv"}>
+                                href={'download/' + this.props.data.search_id + '.full_tsv'}>
                                 Full tabular report
                             </a>
                         </li>
@@ -1107,7 +1107,7 @@ var SideBar = React.createClass({
                         !this.props.data.imported_xml && <li>
                             <a className="download" data-toggle="tooltip"
                                 title="Results in XML format."
-                                href={"download/" + this.props.data.search_id + ".xml"}>
+                                href={'download/' + this.props.data.search_id + '.xml'}>
                                 Full XML report
                             </a>
                         </li>
