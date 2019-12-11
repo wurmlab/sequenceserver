bd781fe2f1e07c67739c34256b0d5dde1beebc70
Iwo Pieniak <ivo.pieniak@gmail.com>
2019-12-11 21:19:46 +0000

    Validation check fix for circos
    Now the accurate number of hits is determined from all the queries.
    Tested with :
    1) 1 Que + 2 hits
    2) 2 Que + 1 hit each
    3) 2 Que + 1 hit + 1 no hit
    4) 3 Que + 1 hit + 2 no hit
    5) 2 Que + 2 hits from single que + 1 no hit
    
    Signed-off-by: Iwo Pieniak <ivo.pieniak@gmail.com>

--- public/js/report.js
+++ public/js/report.js
@@ -259,8 +259,17 @@ var Report = React.createClass({
     },
 
     atLeastTwoHits: function () {
-        var hit_num = (this.state.queries[this.state.queries.length - 1]).number;
-        return (this.state.queries.length >= 1 && hit_num > 1);
+        var hit_arr = [];
+        var hit_num = 0;
+        _.each(this.state.queries, (function (obj) {
+            if (obj.hits.length > 0) {
+                hit_arr.push(obj.hits.length);
+            }
+        }));
+        for (var i = 0; i < hit_arr.length; i++) {
+            hit_num += hit_arr[i];
+        }
+        return (this.state.queries.length > 0 && hit_num > 1);
     },
 
     /**
