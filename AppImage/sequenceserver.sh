a1bbb07e4d870e814997591051217b11f35051a7
Anurag Priyam <priyam@prometheus.qmul.ac.uk>
2019-12-16 12:14:53 +0000

    Reset working directory inside the AppImage
    Signed-off-by: Anurag Priyam <anurag08priyam@gmail.com>

--- AppImage/sequenceserver.sh
+++ AppImage/sequenceserver.sh
@@ -1,3 +1,8 @@
+# cd back to the directory where the AppImage was launched from. AppRun changes
+# the working directory so as to work with binaries that have /usr hard-coded;
+# we don't.
+cd $OWD
+
 # Set GEM_PATH to where we installed gems in the AppImage.
 export GEM_PATH=$APPDIR/var/lib/gems/2.3.0
 
