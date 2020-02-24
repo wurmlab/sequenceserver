a78f0645958d649c40562036936f0ed6d67f700c
Anurag Priyam <anurag08priyam@gmail.com>
2020-02-24 17:10:17 +0000

    Revert Evalue to E value in hspStats
    Signed-off-by: Anurag Priyam <anurag08priyam@gmail.com>

--- public/js/hsp.js
+++ public/js/hsp.js
@@ -67,7 +67,7 @@ export default class HSP extends React.Component {
         line.push(`Score: ${Utils.inTwoDecimal(this.hsp.bit_score)} (${this.hsp.score}), `);
 
         // E value
-        line.push(`Evalue: `); line.push(Utils.inExponential(this.hsp.evalue)); line.push(', ');
+        line.push('E value: '); line.push(Utils.inExponential(this.hsp.evalue)); line.push(', ');
 
         // Identity
         line.push([`Identity: ${Utils.inFraction(this.hsp.identity, this.hsp.length)} (${Utils.inPercentage(this.hsp.identity, this.hsp.length)}), `]);
