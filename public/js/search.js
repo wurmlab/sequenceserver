wurmlab/sequenceserver

commit 0617ccdd8e7f1b9ab4a8a5aa1baf2664d2dfe862

Fix the infinite loop caused on search page on back button or single db
Signed-off-by: Anurag Priyam <anurag08priyam@gmail.com>

public/js/search.js
@@ -647,7 +647,7 @@ var Databases = React.createClass({
 
     handleClick: function (database) {
         var type = this.nselected() ? database.type : '';
-        this.setState({type: type});
+        if (type != this.state.type) this.setState({type: type});
     },
 
     handleToggle: function (toggleState, type) {
@@ -734,10 +734,6 @@ var Databases = React.createClass({
         );
     },
 
-    //shouldComponentUpdate: function (props, state) {
-    //return !(state.type && state.type === this.state.type);
-    //},
-
     componentDidUpdate: function () {
         if (this.databases() && this.databases().length === 1) {
             $('.databases').find('input').prop('checked',true);
@@ -747,10 +743,7 @@ var Databases = React.createClass({
         if (this.props.preSelectedDbs) {
             var selectors = this.props.preSelectedDbs.map(db => `input[value=${db.id}]`);
             $(...selectors).prop('checked',true);
-            setTimeout(() => {
-                this.handleClick(this.props.preSelectedDbs[0]);
-                this.props.preSelectedDbs = null;
-            });
+            this.handleClick(this.props.preSelectedDbs[0]);
         }
         this.props.onDatabaseTypeChanged(this.state.type);
     }

commit 447a7df654dade104132c7862c4bd86fff56ca59

Regression - Clear preSelectedDbs after it is applied
Commit 0617ccdd caused the regression. See 6ba10de2
for why it is important to clear preSelectedDbs.

Signed-off-by: Anurag Priyam <anurag08priyam@gmail.com>

public/js/search.js
@@ -771,6 +771,7 @@ var Databases = React.createClass({
             var selectors = this.props.preSelectedDbs.map(db => `input[value=${db.id}]`);
             $(...selectors).prop('checked',true);
             this.handleClick(this.props.preSelectedDbs[0]);
+            this.props.preSelectedDbs = null;
         }
         this.props.onDatabaseTypeChanged(this.state.type);
     }