wurmlab/sequenceserver

commit 30ae761284908e55eac84cbbafdeb6a4fa6c9ce6

Revert to showing identity of top hit in the per-query hits table
Also change the title of the table back to 'Sequences producing
significant alignments' and of length distribution visualisation
to 'Length distribution of matching sequences'. This renaming is
because 'hit' can either mean the matching database sequence or
the matching region (HSP) to different users in different contexts.

Also, remove Hit#evalue and Hit#identity methods that are no longer
used and rename Hit#score to Hit#total_score for clarity.

close #506

Signed-off-by: Anurag Priyam <anurag08priyam@gmail.com>

public/js/length_distribution.js
@@ -9,7 +9,7 @@ import * as Helpers from './visualisation_helpers';
 
 class Graph {
     static name() {
-        return 'Length distribution of hits';
+        return 'Length distribution of matching sequences';
     }
 
     static className() {

lib/sequenceserver/blast/hit.rb
@@ -21,8 +21,8 @@ module SequenceServer
       # representation.
       def to_json(*args)
         # List all attributes that we want to send to the browser.
-        properties = %i[number id accession title length score identity
-                        qcovs sciname evalue hsps links]
+        properties = %i[number id accession title length total_score
+                        qcovs sciname hsps links]
         properties.inject({}) { |h, k| h[k] = send(k); h }.to_json(*args)
       end
 
@@ -71,21 +71,10 @@ module SequenceServer
       # client.
       ###
 
-      # Returns the sum of scores of all HSPs.
-      def score
+      # Returns the sum of scores of all HSPs. Displayed in the tabular summary
+      # of hits in the HTML report. Should probably be calculated in browser?
+      def total_score
         hsps.map(&:score).reduce(:+)
-      end
-
-      # Returns the sum of identity of all HSPs divided by sum of length of all
-      # HSPs (expressed as percentage).
-      def identity
-        hsps.map(&:identity).reduce(:+) * 100 / hsps.map(&:length).reduce(:+)
-      end
-
-      # Returns the minimum evalue of all HSPs of the Hit. This is shown in the
-      # tabular overview of hits in the HTML report.
-      def evalue
-        hsps.first.evalue
       end
 
       private

public/js/query.js
@@ -111,7 +111,7 @@ var HitsTable = React.createClass({
             <div className="table-hit-overview">
                 <h4 className="caption" data-toggle="collapse" data-target={'#Query_'+this.props.query.number+'HT_'+this.props.query.number}>
                     <i className="fa fa-minus-square-o"></i>&nbsp;
-                    <span>Summary table of hits</span>
+                    <span>Sequences producing significant alignments</span>
                 </h4>
                 <div className="collapsed in"id={'Query_'+ this.props.query.number + 'HT_'+ this.props.query.number}>
                     <table
@@ -123,10 +123,7 @@ var HitsTable = React.createClass({
                             {!this.props.imported_xml && <th width="15%" className="text-right">Query coverage (%)</th>}
                             <th width="10%" className="text-right">Total score</th>
                             <th width="10%" className="text-right">E value</th>
-                            <th width="10%" className="text-right" data-toggle="tooltip"
-                                data-placement="left" title="Total identity of all hsps / total length of all hsps">
-                        Identity (%)
-                            </th>
+                            <th width="10%" className="text-right">Identity (%)</th>
                         </thead>
                         <tbody>
                             {
@@ -147,9 +144,9 @@ var HitsTable = React.createClass({
                                                 </td>
                                             }
                                             {!this.props.imported_xml && <td className="text-right">{hit.qcovs}</td>}
-                                            <td className="text-right">{hit.score}</td>
+                                            <td className="text-right">{hit.total_score}</td>
                                             <td className="text-right">{this.inExponential(hit.hsps[0].evalue)}</td>
-                                            <td className="text-right">{hit.identity}</td>
+                                            <td className="text-right">{this.inPercentage(hit.hsps[0].identity, hit.hsps[0].length)}</td>
                                         </tr>
                                     );
                                 }, this))

commit d83c1721d5580496c0d8571afa8bf36d91ce4f86

Commit 30ae7612 broke length distribution visusalisation; fix it.
Signed-off-by: Anurag Priyam <anurag08priyam@gmail.com>

public/js/length_distribution.js
@@ -129,14 +129,15 @@ class Graph {
             bin.map(function (d,i) {
                 var y1 = bin.length - (i+1);
                 var len_index = _.findIndex(self.query.hits, {length: d});
+                var evalue = self.query.hits[len_index].hsps[0].evalue;
                 var item = {
                     value: d,
                     id: self.query.hits[len_index].id,
-                    evalue: self.query.hits[len_index].evalue,
+                    evalue: evalue,
                     url: '#Query_'+self.query.number+'_hit_'+self.query.hits[len_index].number,
                     y0: y0,
                     y1: y0 += (y1 - y0),
-                    color: Helpers.get_colors_for_evalue(self.query.hits[len_index].evalue,self.query.hits)
+                    color: Helpers.get_colors_for_evalue(evalue,self.query.hits)
                 };
                 inner_data.push(item);
             });