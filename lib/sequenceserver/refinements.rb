dc7816ce5c3d7614cc93169769e1f2cf26012725
Anurag Priyam <anurag.priyam@kws.com>
2022-02-17 13:18:20 +0100

    Fix an embarassing bug introduced in 0b841d49

--- lib/sequenceserver/refinements.rb
+++ lib/sequenceserver/refinements.rb
@@ -6,7 +6,7 @@ class Hash
   def deep_merge(other)
     merge(other) do |key, oldval, newval|
       if oldval.is_a? Hash then
-        merge(oldval, newval)
+        oldval.deep_merge(newval)
       else
         newval
       end

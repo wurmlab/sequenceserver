wurmlab/sequenceserver

commit 0b841d497b0bf08eaaad49c2a3c588ccb68c51c5

Rename Config#merge to Hash#deep_merge
Config#merge was introduced in 2181d290 to deep merge Hashes.
In previous commit, there was a need to also define deep_copy
for Hashes, so we created refinements.rb to monkey patch Hash
class. Now we are moving Config#merge to where it should have
been defined in the first place.

lib/sequenceserver/refinements.rb
@@ -2,4 +2,14 @@ class Hash
   def deep_copy
     Marshal.load Marshal.dump self
   end
+
+  def deep_merge(other)
+    merge(other) do |key, oldval, newval|
+      if oldval.is_a? Hash then
+        merge(oldval, newval)
+      else
+        newval
+      end
+    end
+  end
 end

lib/sequenceserver/config.rb
@@ -15,10 +15,10 @@ module SequenceServer
       @config_file = @data.delete(:config_file)
       if @config_file
         @config_file = File.expand_path(@config_file)
-        @data = merge(parse_config_file, @data)
+        @data = parse_config_file.deep_merge @data
       end
 
-      @data = merge(defaults, @data)
+      @data = defaults.deep_merge @data
 
       if @upgraded
         logger.info 'You are using old configuration syntax. ' \
@@ -85,13 +85,6 @@ module SequenceServer
       end
 
       data
-    end
-
-    # Deep merge Hashes.
-    def merge(target, new_data)
-      target.merge(new_data) do |key, oldval, newval|
-        if oldval.is_a? Hash then merge(oldval, newval) else newval end
-      end
     end
 
     # Parses and returns data from config_file if it exists. Returns {}

commit dc7816ce5c3d7614cc93169769e1f2cf26012725

Fix an embarassing bug introduced in 0b841d49

lib/sequenceserver/refinements.rb
@@ -6,7 +6,7 @@ class Hash
   def deep_merge(other)
     merge(other) do |key, oldval, newval|
       if oldval.is_a? Hash then
-        merge(oldval, newval)
+        oldval.deep_merge(newval)
       else
         newval
       end