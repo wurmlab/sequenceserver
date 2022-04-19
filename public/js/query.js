1cab1900df7afa13bee9800edd24f8a88de4c199
Emmanuel <emmanuelhemarxyll@gmail.com>
2022-04-19 09:01:16 +0100

    refactor query.js to use es6 classes

--- public/js/query.js
+++ public/js/query.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import _ from 'underscore';
 
 import HitsOverview from './hits_overview';
@@ -9,42 +9,39 @@ import Utils from './utils'; // to use as mixin in HitsTable
  * Query component displays query defline, graphical overview, length
  * distribution, and hits table.
  */
-export default React.createClass({
-
+export default class extends Component {
+    // Each update cycle will cause all previous queries to be re-rendered.
+    // We avoid that by implementing shouldComponentUpdate life-cycle hook.
+    // The trick is to simply check if the components has recieved props
+    // before.
+    shouldComponentUpdate() {
+        // If the component has received props before, query property will
+        // be set on it. If it is, we return false so that the component
+        // is not re-rendered. If the query property is not set, we return
+        // true: this must be the first time react is trying to render the
+        // component.
+        return !this.props.query;
+    }
     // Kind of public API //
 
     /**
      * Returns the id of query.
      */
-    domID: function () {
+    domID() {
         return 'Query_' + this.props.query.number;
-    },
+    }
 
-    queryLength: function () {
+    queryLength() {
         return this.props.query.length;
-    },
+    }
 
     /**
      * Returns number of hits.
      */
-    numhits: function () {
+    numhits() {
         return this.props.query.hits.length;
-    },
-
-    // Life cycle methods //
-
-    render: function () {
-        return (
-            <div className="resultn" id={this.domID()}
-                data-query-len={this.props.query.length}
-                data-algorithm={this.props.program}>
-                { this.headerJSX() }
-                { this.numhits() && this.hitsListJSX() || this.noHitsJSX() }
-            </div>
-        );
-    },
-
-    headerJSX: function () {
+    }
+    headerJSX() {
         var meta = `length: ${this.queryLength().toLocaleString()}`;
         if (this.props.showQueryCrumbs) {
             meta = `query ${this.props.query.number}, ` + meta;
@@ -54,45 +51,45 @@ export default React.createClass({
                 <strong>Query=&nbsp;{this.props.query.id}</strong>&nbsp;
                 {this.props.query.title}
             </h3>
-            <span className="label label-reset pos-label">{ meta }</span>
+            <span className="label label-reset pos-label">{meta}</span>
         </div>;
-    },
+    }
 
-    hitsListJSX: function () {
+    hitsListJSX() {
         return <div className="section-content">
             <HitsOverview key={'GO_' + this.props.query.number} query={this.props.query} program={this.props.program} collapsed={this.props.veryBig} />
             <LengthDistribution key={'LD_' + this.props.query.id} query={this.props.query} algorithm={this.props.program} collapsed="true" />
             <HitsTable key={'HT_' + this.props.query.number} query={this.props.query} imported_xml={this.props.imported_xml} />
         </div>;
-    },
+    }
 
-    noHitsJSX: function () {
+    noHitsJSX() {
         return <div className="section-content">
             <strong> ****** No hits found ****** </strong>
         </div>;
-    },
-
-    // Each update cycle will cause all previous queries to be re-rendered.
-    // We avoid that by implementing shouldComponentUpdate life-cycle hook.
-    // The trick is to simply check if the components has recieved props
-    // before.
-    shouldComponentUpdate: function () {
-        // If the component has received props before, query property will
-        // be set on it. If it is, we return false so that the component
-        // is not re-rendered. If the query property is not set, we return
-        // true: this must be the first time react is trying to render the
-        // component.
-        return !this.props.query;
     }
-});
-
+    render() {
+        return (
+            <div className="resultn" id={this.domID()}
+                data-query-len={this.props.query.length}
+                data-algorithm={this.props.program}>
+                {this.headerJSX()}
+                {this.numhits() && this.hitsListJSX() || this.noHitsJSX()}
+            </div>
+        );
+    }
+}
 /**
  * Renders summary of all hits per query in a tabular form.
  */
-var HitsTable = React.createClass({
-    mixins: [Utils],
-    render: function () {
-        var hasName = _.every(this.props.query.hits, function(hit) {
+
+class HitsTable extends Component {
+    constructor(props) {
+        super(props);
+        this.mixins = [Utils];
+    }
+    render() {
+        var hasName = _.every(this.props.query.hits, function (hit) {
             return hit.sciname !== '';
         });
 
@@ -109,11 +106,11 @@ var HitsTable = React.createClass({
 
         return (
             <div className="table-hit-overview">
-                <h4 className="caption" data-toggle="collapse" data-target={'#Query_'+this.props.query.number+'HT_'+this.props.query.number}>
+                <h4 className="caption" data-toggle="collapse" data-target={'#Query_' + this.props.query.number + 'HT_' + this.props.query.number}>
                     <i className="fa fa-minus-square-o"></i>&nbsp;
                     <span>Sequences producing significant alignments</span>
                 </h4>
-                <div className="collapsed in"id={'Query_'+ this.props.query.number + 'HT_'+ this.props.query.number}>
+                <div className="collapsed in" id={'Query_' + this.props.query.number + 'HT_' + this.props.query.number}>
                     <table
                         className="table table-hover table-condensed tabular-view ">
                         <thead>
@@ -157,4 +154,4 @@ var HitsTable = React.createClass({
             </div>
         );
     }
-});
+}
