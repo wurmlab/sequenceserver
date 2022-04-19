e7af11d7a6ac9c14ca75bb688ccfa7505412fe16
Emmanuel <emmanuelhemarxyll@gmail.com>
2022-04-19 02:25:25 +0100

    convert databases_tree.js to es6 class

--- public/js/databases_tree.js
+++ public/js/databases_tree.js
@@ -1,36 +1,60 @@
-import React from 'react';
+import React, { Component } from 'react';
 import _ from 'underscore';
 import Jstree from 'vakata/jstree';
 
-export default React.createClass({
-    getInitialState: function () {
-        return { type: '' };
-    },
 
-    databases: function (category) {
+export default class extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { type: '' };
+        this.databases = this.databases.bind(this);
+        this.categories = this.categories.bind(this);
+        this.handleClick = this.handleClick.bind(this);
+        this.handleLoadTree = this.handleLoadTree.bind(this);
+        this.renderDatabases = this.renderDatabases.bind(this);
+        this.renderDatabaseSearch = this.renderDatabaseSearch.bind(this);
+        this.renderDatabaseTree = this.renderDatabaseTree.bind(this);
+        this.renderDatabase = this.renderDatabase.bind(this);
+    }
+    componentDidUpdate() {
+        if (this.databases() && this.databases().length === 1) {
+            $('.databases').find('input').prop('checked', true);
+            this.handleClick(this.databases()[0]);
+        }
+
+        if (this.props.preSelectedDbs) {
+            var selectors = this.props.preSelectedDbs.map(db => `input[value=${db.id}]`);
+            $(...selectors).prop('checked', true);
+            this.handleClick(this.props.preSelectedDbs[0]);
+            this.props.preSelectedDbs = null;
+        }
+        this.props.onDatabaseTypeChanged(this.state.type);
+    }
+
+    databases(category) {
         var databases = this.props.databases;
         if (category) {
             databases = _.select(databases, database => database.type === category);
         }
 
         return _.sortBy(databases, 'title');
-    },
+    }
 
-    nselected: function () {
+    nselected() {
         return $('input[name="databases[]"]:checked').length;
-    },
+    }
 
-    categories: function () {
+    categories() {
         return _.uniq(_.map(this.props.databases,
             _.iteratee('type'))).sort();
-    },
+    }
 
-    handleClick: function (database) {
+    handleClick(database) {
         var type = this.nselected() ? database.type : '';
-        if (type != this.state.type) this.setState({type: type});
-    },
+        if (type != this.state.type) this.setState({ type: type });
+    }
 
-    handleLoadTree: function (category) {
+    handleLoadTree(category) {
         var tree_id = '#' + category + '_database_tree';
         // hack that is needed to sync the selected tree db with the hidden main db
         window.jstree_node_change_timeout = null;
@@ -42,38 +66,37 @@ export default React.createClass({
                 window.jstree_node_change_timeout = null;
             }
 
-            window.jstree_node_change_timeout = setTimeout(function(){
+            window.jstree_node_change_timeout = setTimeout(function () {
                 // uncheck all input
                 $('div#database_list input[type="checkbox"]:checked').click();
-                setTimeout(function(){
+                setTimeout(function () {
                     // get all selected tree dbs. Also includes folders. Therefore, the id must have a length of 32
                     // this id is used to find the corresponding element from the hidden main form
                     var selected = $(tree_id).jstree('get_selected').filter(selected => selected.length == 32);
-                    $.each(selected, function( index, value ) {
+                    $.each(selected, function (index, value) {
                         // select hidden element to trigger original sequenceserver behavior, like blast algorithm, ...
-                        $('input[value="'+ value + '"]').click();
+                        $('input[value="' + value + '"]').click();
                     });
                 }, 100);
             }, 100);
         });
 
         $(tree_id).jstree({
-            'core' : {
+            'core': {
                 'data': this.props.tree[category]
             },
-            'plugins' : [ 'checkbox', 'search', 'sort' ],
-            'checkbox' : {
-                'keep_selected_style' : false
+            'plugins': ['checkbox', 'search', 'sort'],
+            'checkbox': {
+                'keep_selected_style': false
             }
         });
-    },
+    }
 
-    handleTreeSearch: function(category, tree_id, search_id) {
+    handleTreeSearch(category, tree_id, search_id) {
         var search_for = $('#' + search_id).val();
         $('#' + tree_id).jstree(true).search(search_for);
-    },
-
-    handleToggle: function (toggleState, type) {
+    }
+    handleToggle(toggleState, type) {
         switch (toggleState) {
         case '[Select all]':
             $(`.${type} .database input:not(:checked)`).click();
@@ -82,27 +105,18 @@ export default React.createClass({
             $(`.${type} .database input:checked`).click();
             break;
         }
-    },
-
-    render: function () {
-        return (
-            <div className='form-group databases-container'>
-                { _.map(this.categories(), this.renderDatabases) }
-            </div>
-        );
-    },
-
-    renderDatabases: function (category) {
+    }
+    renderDatabases(category) {
         // Panel name and column width.
         var panelTitle = category[0].toUpperCase() +
             category.substring(1).toLowerCase() + ' databases';
-        var columnClass = this.categories().length === 1 ?  'col-md-12' :
+        var columnClass = this.categories().length === 1 ? 'col-md-12' :
             'col-md-6';
 
         // Toggle button.
         var toggleState = '[Select all]';
         var toggleClass = 'btn-link';
-        var toggleShown = this.databases(category).length > 1 ;
+        var toggleShown = this.databases(category).length > 1;
         var toggleDisabled = this.state.type && this.state.type !== category;
         if (toggleShown && toggleDisabled) toggleClass += ' disabled';
         if (!toggleShown) toggleClass += ' hidden';
@@ -112,24 +126,24 @@ export default React.createClass({
 
         // JSX.
         return (
-            <div className={columnClass} key={'DB_'+category}>
+            <div className={columnClass} key={'DB_' + category}>
                 <div className='panel panel-default' id='database_list'>
                     <div className='panel-heading'>
-                        <h4 style={{display: 'inline'}}>{panelTitle}</h4> &nbsp;&nbsp;
+                        <h4 style={{ display: 'inline' }}>{panelTitle}</h4> &nbsp;&nbsp;
                         {
                             this.renderDatabaseSearch(category)
                         }
-                        <button type='button' className={toggleClass} disabled={toggleDisabled} style={{display: 'none'}}
-                            onClick={ function () { this.handleToggle(toggleState, category); }.bind(this) }>
+                        <button type='button' className={toggleClass} disabled={toggleDisabled} style={{ display: 'none' }}
+                            onClick={function () { this.handleToggle(toggleState, category); }.bind(this)}>
                             {toggleState}
                         </button>
                     </div>
-                    <ul className={'list-group databases ' + category} style={{display: 'none'}}>
+                    <ul className={'list-group databases ' + category} style={{ display: 'none' }}>
                         {
-                            _.map(this.databases(category), _.bind(function (database,index) {
+                            _.map(this.databases(category), _.bind(function (database, index) {
                                 return (
-                                    <li className='list-group-item' key={'DB_'+category+index}>
-                                        { this.renderDatabase(database) }
+                                    <li className='list-group-item' key={'DB_' + category + index}>
+                                        {this.renderDatabase(database)}
                                     </li>
                                 );
                             }, this))
@@ -141,9 +155,9 @@ export default React.createClass({
                 }
             </div>
         );
-    },
+    }
 
-    renderDatabaseSearch: function (category) {
+    renderDatabaseSearch(category) {
         var tree_id = category + '_database_tree';
         var search_id = tree_id + '_search';
 
@@ -157,11 +171,11 @@ export default React.createClass({
                     }
             ></input>
         );
-    },
+    }
 
-    renderDatabaseTree: function (category) {
-        var tree_id = category + '_database_tree';
-        var data = this.props.tree[category];
+    renderDatabaseTree(category) {
+        const tree_id = category + '_database_tree';
+        const data = this.props.tree[category];
 
         return (
             <div
@@ -176,9 +190,9 @@ export default React.createClass({
             >
             </div>
         );
-    },
+    }
 
-    renderDatabase: function (database) {
+    renderDatabase(database) {
         var disabled = this.state.type && this.state.type !== database.type;
 
         return (
@@ -192,24 +206,17 @@ export default React.createClass({
                             _.bind(function () {
                                 this.handleClick(database);
                             }, this)
-                        }/>
+                        } />
                 {' ' + (database.title || database.name)}
             </label>
         );
-    },
-
-    componentDidUpdate: function () {
-        if (this.databases() && this.databases().length === 1) {
-            $('.databases').find('input').prop('checked',true);
-            this.handleClick(this.databases()[0]);
-        }
-
-        if (this.props.preSelectedDbs) {
-            var selectors = this.props.preSelectedDbs.map(db => `input[value=${db.id}]`);
-            $(...selectors).prop('checked',true);
-            this.handleClick(this.props.preSelectedDbs[0]);
-            this.props.preSelectedDbs = null;
-        }
-        this.props.onDatabaseTypeChanged(this.state.type);
     }
-});
+
+    render() {
+        return (
+            <div className='form-group databases-container'>
+                {_.map(this.categories(), this.renderDatabases)}
+            </div>
+        );
+    }
+}
