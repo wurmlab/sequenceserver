0de18d4c3736d3f93706e41f1c827a35d22fd698
Carlo Kroll <70138474+iQuxLE@users.noreply.github.com>
2022-04-29 10:37:49 +0100

    Create job_spec.rb

---
+++ spec/job_spec.rb
@@ -0,0 +1,85 @@
+require 'spec_helper'
+require 'sequenceserver/job'
+require 'sequenceserver/blast/job'
+require 'rack/test'
+
+# Test Job class from module SequenceServer
+module SequenceServer
+    describe 'Job' do
+        my_job = Job.new
+
+        it 'should have an id' do
+            expect(my_job.id).to be_kind_of(String)
+        end
+
+        it 'should have a time of submission' do
+            expect(my_job.submitted_at).to be_kind_of(Time)
+        end
+
+        it 'should have an false exitstatus' do
+            expect(my_job.done?).to be_falsey
+        end
+
+        it 'should not be completed yet' do
+            expect(my_job.completed_at).to be(nil)
+        end
+
+        it 'should have a job directory' do
+            expect(my_job.dir).to start_with("/")
+        end 
+
+        # Test Job creation, params and methods 
+        ENV['RACK_ENV'] = 'test'
+        include Rack::Test::Methods
+    
+        before do
+            SequenceServer.init(database_dir: "#{__dir__}/database/v5/sample")
+
+            @params = {
+                sequence: 'AGCTAGCTAGCT', 
+                databases: [Database.first.id],
+                method: (Database.first.id == 'protein' ? 'blastp' : 'blastn'),
+            }
+        end
+
+        context 'with params' do
+            let(:test_job){test_job = Job.create(@params)}
+
+            it 'should create a job.yaml' do
+                expect("#{__dir__}/dotdir/*/job.yaml").not_to be_empty
+            end
+
+            it 'should create a stderr' do
+                expect("#{__dir__}/dotdir/*/stderr").not_to be_empty
+            end
+
+            it 'should create a stdout' do
+                expect("#{__dir__}/dotdir/*/stdout").not_to be_empty
+            end
+
+            it 'should have total database length' do
+                total_database_length = test_job.ncharacters_total #How do I call this database?
+                expect(total_database_length).to be_a Integer
+            end
+                
+            it 'should have a query length of 12' do
+                test_job.query_size
+                expect(test_job.query_size).to eq(12)
+            end
+
+            it 'should be able to store a sequence in query.fa' do
+                test_job.send("store", 'query.fa', 'ACTA')
+                expect(test_job.query_size).to eq(4)
+                expect("#{__dir__}/dotdir/*/query.fa").not_to be_empty
+            end
+
+            it 'should have a command' do
+                expect(test_job.command).to start_with('blastn -db')
+            end
+            
+            it 'should have a method blastn when AGCTAGCTAGCT ' do
+                expect(@params[:method]).to eq('blastn')
+            end
+        end
+    end
+end
