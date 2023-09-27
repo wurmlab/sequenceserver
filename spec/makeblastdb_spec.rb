65cf5129925ce5baf6d753e90ec3e353a81e0db6
Tadas Tamosauskas <tadastamo@gmail.com>
2023-09-27 17:07:42 +0100

    Convert spec/makeblastdb_spec.rb from deprecated Rspec syntax

--- spec/makeblastdb_spec.rb
+++ spec/makeblastdb_spec.rb
@@ -75,45 +75,40 @@ module SequenceServer
     end
 
     it 'can tell FASTA file' do
-      makeblastdb.send(:probably_fasta?, text_file).should be_falsey
-      makeblastdb.send(:probably_fasta?, binary_file).should be_falsey
-      makeblastdb.send(:probably_fasta?, fasta_file_prot_seqs).should be_truthy
-      makeblastdb.send(:probably_fasta?, fasta_file_nucl_seqs).should be_truthy
+      expect(makeblastdb.send(:probably_fasta?, text_file)).to be_falsey
+      expect(makeblastdb.send(:probably_fasta?, binary_file)).to be_falsey
+      expect(makeblastdb.send(:probably_fasta?, fasta_file_prot_seqs)).to be_truthy
+      expect(makeblastdb.send(:probably_fasta?, fasta_file_nucl_seqs)).to be_truthy
     end
 
     it 'can tell type of sequences in FASTA file' do
-      makeblastdb.send(:guess_sequence_type_in_fasta, fasta_file_prot_seqs).should eq :protein
-      makeblastdb.send(:guess_sequence_type_in_fasta, fasta_file_nucl_seqs).should eq :nucleotide
+      expect(makeblastdb.send(:guess_sequence_type_in_fasta, fasta_file_prot_seqs)).to eq :protein
+      expect(makeblastdb.send(:guess_sequence_type_in_fasta, fasta_file_nucl_seqs)).to eq :nucleotide
     end
 
     it 'can tell FASTA files that are yet to be made into a BLAST+ database' do
       makeblastdb.instance_variable_set(:@database_dir, database_dir_unformatted)
-      makeblastdb.scan.should be_truthy
+      expect(makeblastdb.scan).to be_truthy
     end
 
     it 'can tell databases that require reformatting' do
       # Control: shouldn't report sample v5 databases as requiring reformatting.
       makeblastdb.instance_variable_set(:@database_dir, database_dir_v5)
-      makeblastdb.scan.should be_falsey
+      expect(makeblastdb.scan).to be_falsey
 
       # Databases created using blastdb_aliastool don't require reformatting either.
       makeblastdb.instance_variable_set(:@database_dir, database_dir_blastdb_aliastool)
-      makeblastdb.scan.should be_falsey
+      expect(makeblastdb.scan).to be_falsey
 
       # Databases created without -parse_seqids option don't require reformatting either.
       # We disable 'sequence download' link instead.
       makeblastdb.instance_variable_set(:@database_dir, database_dir_without_parse_seqids)
-      makeblastdb.scan.should be_falsey
+      expect(makeblastdb.scan).to be_falsey
 
       # v4 databases require reformatting.
       makeblastdb.instance_variable_set(:@database_dir, database_dir_v4)
-      makeblastdb.scan.should be_truthy
+      expect(makeblastdb.scan).to be_truthy
     end
-
-    # it 'can make BLAST+ database from a FASTA file' do
-    #   Database._make_blast_database(*data_for_makeblastdb).should be_truthy
-    #   system "rm #{makeblastdb_result_pattern}"
-    # end
 
     it 'can make intelligent database name suggestions' do
       db_name_pairs = [['Si_gnf.fasta', 'Si gnf'],
@@ -122,7 +117,7 @@ module SequenceServer
                        ['S_inv.x.small.2.5.nucl.fa', 'S inv x small 2.5 nucl'],
                        ['Sinvicta2-2-3.prot.fasta', 'Sinvicta 2-2-3 prot']]
       db_name_pairs.each do |db|
-        makeblastdb.send(:make_db_title, db[0]).should eql(db[1])
+        expect(makeblastdb.send(:make_db_title, db[0])).to eql(db[1])
       end
     end
 
@@ -130,9 +125,9 @@ module SequenceServer
       sample_name1 = '/home/ben/pd.ben/sequenceserver/db/nr'
       sample_name2 = '/home/ben/pd.ben/sequenceserver/db/nr.00'
       sample_name3 = '/home/ben/pd.ben/sequenceserver/db/img3.5.finished.faa.01'
-      makeblastdb.send(:multipart_database_name?, sample_name1).should be_falsey
-      makeblastdb.send(:multipart_database_name?, sample_name2).should be_truthy
-      makeblastdb.send(:multipart_database_name?, sample_name3).should be_truthy
+      expect(makeblastdb.send(:multipart_database_name?, sample_name1)).to be_falsey
+      expect(makeblastdb.send(:multipart_database_name?, sample_name2)).to be_truthy
+      expect(makeblastdb.send(:multipart_database_name?, sample_name3)).to be_truthy
     end
   end
 end
