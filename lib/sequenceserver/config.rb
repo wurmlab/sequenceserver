f2cf7d099a12f2015e4168509136d9d66a9d77cc
Tadas Tamosauskas <tadastamo@gmail.com>
2023-10-04 16:35:30 +0100

    [Performance] Add --optimistic flag that skips DB checks
    By default, when running SequenceServer.init i.e. when starting
    the web server or runninc CLI commands, it will scan the entire
    database_dir entries to find databases that use older (v4) version
    or have been created without parse_seqids. If this is the case
    it will log a warning.
    
    If all databases are formatted correctly and the user knows it,
    running SequenceServer with --optimistic can have a significant
    impact on reducing startup times. With vast databse directories
    this can be in tens of seconds.
    
    This is because parse_seqids in particular does a lot of IO calls
    to scan database directories to determine if seqids metadata files
    are missing for each database.

--- lib/sequenceserver/config.rb
+++ lib/sequenceserver/config.rb
@@ -132,7 +132,9 @@ module SequenceServer
         job_lifetime: 43_200,
         # Set cloud_share_url to 'disabled' to disable the cloud sharing feature
         cloud_share_url: 'https://share.sequenceserver.com/api/v1/shared-job',
-        large_result_warning_threshold: 250 * 1024 * 1024
+        # Warn in the UI before rendering results larger than this value
+        large_result_warning_threshold: 250 * 1024 * 1024,
+        optimistic: false # Faster, but does not perform DB compatibility checks
       }
     end
   end
