8502ba595ca54978df2ea16ebb754ccd852807e0
Tadas Tamosauskas <tadastamo@gmail.com>
2024-04-04 13:41:01 +0100

    Style modal and stop relying on bootstrap js to show it

--- public/js/options.js
+++ public/js/options.js
@@ -43,8 +43,13 @@ export class Options extends Component {
         const method = this.state.method.toLowerCase();
         // hide options for other algorithms and only show for selected algorithm
         for (const id of ids) {
-            $(`#${id}`)[id === method ? 'show' : 'hide']();
+            if (id === method) {
+                document.getElementById(id).classList.remove('hidden')
+            } else {
+                document.getElementById(id).classList.add('hidden');
+            }
         }
+        document.querySelector('[data-help-modal]').classList.remove('hidden')
     }
     render() {
         var classNames = 'form-control';
@@ -53,14 +58,14 @@ export class Options extends Component {
         }
         return (
             <div className="flex-grow flex items-center space-x-2">
-                <label className="control-label" htmlFor="advanced">
+                <label className="flex" htmlFor="advanced">
                     Advanced parameters:
                     {/* only show link to advanced parameters if blast method is known */}
-                    {this.state.method && <sup style={{ marginLeft: '2px' }}>
+                    {this.state.method && <sup className="mx-1 text-seqblue">
                         <a href=''
                             onClick={this.showAdvancedOptions}
                             data-toggle="modal" data-target="#help">
-                            <i className="fa fa-question-circle"></i>
+                            <svg className="w-3 h-4 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM169.8 165.3c7.9-22.3 29.1-37.3 52.8-37.3h58.3c34.9 0 63.1 28.3 63.1 63.1c0 22.6-12.1 43.5-31.7 54.8L280 264.4c-.2 13-10.9 23.6-24 23.6c-13.3 0-24-10.7-24-24V250.5c0-8.6 4.6-16.5 12.1-20.8l44.3-25.4c4.7-2.7 7.6-7.7 7.6-13.1c0-8.4-6.8-15.1-15.1-15.1H222.6c-3.4 0-6.4 2.1-7.5 5.3l-.4 1.2c-4.4 12.5-18.2 19-30.6 14.6s-19-18.2-14.6-30.6l.4-1.2zM224 352a32 32 0 1 1 64 0 32 32 0 1 1 -64 0z"/></svg>
                         </a>
                     </sup>}
                 </label>
