f6f156966335f6e05b945ae9405162c2f90527b3
3lviend <henryss_10@yahoo.com>
2024-09-06 14:03:31 +0700

    Fix share to cloude modal styles & update line number bar styles on view sequence modal

--- public/js/share_url.js
+++ public/js/share_url.js
@@ -10,8 +10,8 @@ const ShareURLComponent = ({ querydb, program, queryLength, url }) => {
     };
 
     return (
-        <div className="share-url-component">
-            <input className="w-full" name="shareableUrl" type="text" value={url} readOnly />
+        <div className="px-6 share-url-component">
+            <input className="w-full mb-3" name="shareableUrl" type="text" value={url} readOnly />
             <div className="py-4 flex justify-between">
                 <button className="py-2 px-3 border border-transparent rounded-md shadow-sm text-white bg-seqblue hover:bg-seqorange focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-seqorange" onClick={copyToClipboard}>{copied ? 'Copied!' : 'Copy to Clipboard'}</button>
                 <a href={asMailtoHref(querydb, program, queryLength, url, true)}>Share via email</a>
