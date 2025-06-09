d14421bebcdfbc0512c73d7faba78f6ef4338500
Tadas Tamosauskas <tadastamo@gmail.com>
2025-06-09 13:45:44 +0100

    Refactor collapse preferences to store both open and closed states.
    If we want an element to be closed by default, in
    the previous implementation we would not have been
    able to store that state - if a user opened an
    element it would remove it from the collapsed
    elements array, but would not keep it open on the
    consecutive load, because it would fall back to
    the default collapsed state.
    
    This change stores both open and closed states,
    and uses a new storage key to store the states
    to avoid breaking changes due to old storage.
    
    The collapse preferences are now stored in the
    localStorage as a JSON object, with the key being
    the name of the element and the value being a boolean
    indicating whether the element is collapsed or not.

--- public/js/collapse_preferences.js
+++ public/js/collapse_preferences.js
@@ -2,29 +2,23 @@ export default class CollapsePreferences {
     constructor(component, defaultCollapsed = false) {
         this.component = component;
         this.defaultCollapsed = defaultCollapsed;
-        this.collapsePreferences = JSON.parse(localStorage.getItem('collapsePreferences')) || [];
+        this.storageKey = 'sequenceServerCollapseStates';
+        this.collapseStates = JSON.parse(localStorage.getItem(this.storageKey)) || {};
     }
 
     toggleCollapse() {
         let currentlyCollapsed = this.component.state.collapsed;
         this.component.setState({ collapsed: !currentlyCollapsed });
 
-        if (currentlyCollapsed) {
-            localStorage.setItem('collapsePreferences', JSON.stringify(this.collapsePreferences.filter((name) => name !== this.component.name)));
-        } else {
-            let uniqueCollapsePreferences = [... new Set(this.collapsePreferences.concat([this.component.name]))];
-            localStorage.setItem('collapsePreferences', JSON.stringify(uniqueCollapsePreferences));
-        }
+        this.collapseStates[this.component.name] = !currentlyCollapsed;
+        localStorage.setItem(this.storageKey, JSON.stringify(this.collapseStates));
     }
 
     preferenceStoredAsCollapsed() {
-        if (this.collapsePreferences.includes(this.component.name)) {
-            return true;
+        if (this.component.name in this.collapseStates) {
+            return this.collapseStates[this.component.name];
         }
-        if (this.collapsePreferences.length === 0 && this.defaultCollapsed) {
-            return true;
-        }
-        return false;
+        return this.defaultCollapsed;
     }
 
     renderCollapseIcon() {
